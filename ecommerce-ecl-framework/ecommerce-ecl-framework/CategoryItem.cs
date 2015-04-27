8aef7842e8a7b9c1df97b8376f007af93f756f0e
Niclas Cedermalm <niclas@cedermalm.se>
2015-04-27 16:19:52 +0200

    First version of the generic E-Commerce ECL Framework.

---
+++ ecommerce-ecl-framework/ecommerce-ecl-framework/CategoryItem.cs
@@ -0,0 +1,147 @@
+﻿using SDL.ECommerce.Ecl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tridion.ExternalContentLibrary.V2;
+
+namespace SDL.ECommerce.Ecl
+{
+    public class CategoryItem : IContentLibraryListItem, IContentLibraryItem
+    {
+        protected readonly IEclUri id;
+        protected readonly Category category;
+
+        public CategoryItem(int publicationId, Category category)
+        {
+            this.category = category;
+            this.id = EclProvider.HostServices.CreateEclUri(publicationId, EclProvider.MountPointId, 
+                category.CategoryId != null ? category.CategoryId : "0", DisplayTypeId, EclItemTypes.Folder);
+        }
+
+        public bool CanGetUploadMultimediaItemsUrl
+        {
+            get { return false; }
+        }
+
+        public bool CanSearch
+        {
+            get { return false; }
+        }
+
+        public string DisplayTypeId
+        {
+            get { return "category"; }
+        }
+
+        public string IconIdentifier
+        {
+            get { return null; }
+        }
+
+        public IEclUri Id
+        {
+            get { return id; }
+        }
+
+        public bool IsThumbnailAvailable
+        {
+            get { return false; }
+        }
+
+        public DateTime? Modified
+        {
+            get { return null; }
+        }
+
+        public string ThumbnailETag
+        {
+            get { return null; }
+        }
+
+        public string Title
+        {
+            get { return this.category.Title; }
+            set { throw new NotSupportedException(); }
+        }
+
+        // allow override of dispatch
+        public virtual string Dispatch(string command, string payloadVersion, string payload, out string responseVersion)
+        {
+            throw new NotSupportedException();
+        }
+
+        public IEclUri ParentId
+        {
+            get
+            {
+                if (category.Parent == null)
+                {
+                    //return EclProvider.HostServices.CreateEclUri(Id.PublicationId, Id.MountPointId);
+                    return EclProvider.HostServices.CreateEclUri(Id.PublicationId, Id.MountPointId, "Products", "category", EclItemTypes.Folder);
+                }
+                else
+                {
+                    // return parent folder
+                    return EclProvider.HostServices.CreateEclUri(
+                        Id.PublicationId,
+                        Id.MountPointId,
+                        category.Parent.CategoryId != null ? category.Parent.CategoryId : "0", 
+                        "category",
+                        EclItemTypes.Folder);
+                }
+            }
+        }
+
+        public string ModifiedBy
+        {
+            get { return CreatedBy; }
+        }
+
+        public string CreatedBy
+        {
+            get { return null; }
+        }
+
+        public DateTime? Created
+        {
+            get { return null; }
+        }
+
+        public string MetadataXml
+        {
+            get { return null; } 
+            set { throw new NotSupportedException(); }
+        }
+
+        public ISchemaDefinition MetadataXmlSchema
+        {
+            get
+            {
+               return null;
+            }
+        }
+
+        public bool CanUpdateTitle
+        {
+            get { return false; }
+        }
+
+        public bool CanGetViewItemUrl
+        {
+            get { return true; }
+        }
+
+        public bool CanUpdateMetadataXml
+        {
+            get { return false; }
+        }
+
+        public IContentLibraryItem Save(bool readback)
+        {
+            // as saving isn't supported, the result of saving is always the item itself
+            return readback ? this : null;
+        }
+    }
+}
---
+++ ecommerce-ecl-framework/ecommerce-ecl-framework/UnitTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SDL.ECommerce.Ecl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telenor.ECommerce.Ecl
+{
+     
+    [TestClass]   
+    public class UnitTests
+    {
+        /*
+        [TestMethod]
+        public void TestGetCategories()
+        {
+            Console.WriteLine("Getting root categories...");
+            var catalog = new TelenorProductCatalog("http://preview:8000");
+            var rootCategory = catalog.GetAllCategories();
+            PrintCategory(rootCategory,0);
+        }
+
+        void PrintCategory(Category category, int level)
+        {
+            for (int i = 0; i < level; i++ )
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("Category ID=" + category.CategoryId);
+            foreach ( var subCategory in category.Categories )
+            {
+                PrintCategory(subCategory, level+1);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestGetProduct()
+        {
+            Console.WriteLine("Get product...");
+            var catalog = new TelenorProductCatalog("http://preview:8000");
+            TelenorProduct product = (TelenorProduct) catalog.GetProduct("iphone-6-plus-16gb");
+            Console.WriteLine("Product ID=" + product.Id + " Name=" + product.Name + " Price w. subscription=" + product.PriceWithSubscription.PriceWithoutVat + " Available From=" + product.AvailableFrom);
+
+            Console.WriteLine("Listing all products in category 'apple'...");
+            var productIds = catalog.GetProductIds("apple");
+            foreach ( var productId in productIds )
+            {
+                var catProduct = catalog.GetProduct(productId);
+                Console.WriteLine("Product ID=" + catProduct.Id + " Name=" + catProduct.Name);
+            }
+        }
+         * */
+
+    }
+}
