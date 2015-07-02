a972d3ca1f40a370ed98219641515b299beae92f
rpannekoek <rpannekoek@sdl.com>
2015-07-02 16:09:01 +0200

    TSI-820 Initial version of a Test Module with some Test Views (Page/Region/Entity) and Test Entity Models.

---
+++ webapp-net/Test/Models/TestEntityModel2.cs
@@ -0,0 +1,19 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    public class TestEntityModel2 : EntityModel
+    {
+        public double TestProperty1
+        {
+            get;
+            set;
+        }
+
+        public TestEntityModel3 TestProperty2
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ webapp-net/Test/TestAreaRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Sdl.Web.Common.Models;
+using Sdl.Web.Modules.Test.Models;
+using Sdl.Web.Mvc.Configuration;
+
+namespace Sdl.Web.Modules.Test
+{
+    public class TestAreaRegistration : BaseAreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Test";
+            }
+        }
+
+        protected override void RegisterAllViewModels()
+        {
+            // Page Views
+            RegisterViewModel("TestPage1", typeof(PageModel), "Page");
+
+            // Region Views
+            RegisterViewModel("TestRegion1", typeof(RegionModel), "Region");
+
+            // Entity Views
+            RegisterViewModel("TestEntity1", typeof(TestEntityModel1));
+
+            // Entity Models without associated View
+            RegisterViewModel(typeof(TestEntityModel2));
+            RegisterViewModel(typeof(TestEntityModel3));
+        }
+    }
+}
