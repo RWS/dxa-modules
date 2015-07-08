1fe5c11dd779eb12be0541e75d0391b986e4d2a7
Andrii Dosenko <adosenko@sdl.com>
2015-07-08 12:45:46 +0300

    added cases for DxaEntities methods

--- webapp-net/Test/TestAreaRegistration.cs
+++ webapp-net/Test/TestAreaRegistration.cs
@@ -27,6 +27,7 @@ namespace Sdl.Web.Modules.Test
             RegisterViewModel("TestRegion2", typeof(RegionModel), "Region");
             RegisterViewModel("TestRegion3", typeof(RegionModel), "Region");
             RegisterViewModel("TestRegionMarkup", typeof(RegionModel), "Region");
+            RegisterViewModel("TestRegionEntity", typeof(RegionModel), "Region");
 
             // Entity Views
             RegisterViewModel("TestEntity1", typeof(TestEntityModel1));
