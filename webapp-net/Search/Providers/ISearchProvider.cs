5259174764f34d168795f1fd5af0354025793082
rpannekoek <rpannekoek@sdl.com>
2015-08-27 16:45:11 +0200

    TSI-12 Initial commit of new DXA 1.1 Search architecture prepared for AWS CloudSearch (excl. Suggest functionality).

---
+++ webapp-net/Search/Providers/ISearchProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Sdl.Web.Common.Configuration;
+using Sdl.Web.Modules.Search.Models;
+
+namespace Sdl.Web.Modules.Search.Providers
+{
+    public interface ISearchProvider
+    {
+        void ExecuteQuery(SearchQuery searchQuery, Type resultType, Localization localization);
+    }
+}
---
+++ webapp-net/Search/Providers/SolrProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+using Sdl.Web.Common.Logging;
+using SI4T.Query.Models;
+
+namespace Sdl.Web.Modules.Search.Providers
+{
+    public class SolrProvider : SI4TSearchProvider
+    {
+        protected override SearchResults ExecuteQuery(string searchIndexUrl, NameValueCollection parameters)
+        {
+            using (new Tracer(searchIndexUrl, parameters))
+            {
+                SI4T.Query.Solr.Connection solrConnection = new SI4T.Query.Solr.Connection(searchIndexUrl);
+                return solrConnection.ExecuteQuery(parameters);
+            }
+        }
+    }
+}
