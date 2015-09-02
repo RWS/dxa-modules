30662bc6e47ab44a57ed4eccb8dde789d0ff33f4
Bart Koopman <bkoopman@sdl.com>
2015-09-02 11:30:45 +0200

    Added SmartTarget module sources and references

---
+++ webapp-net/SmartTarget/SmartTargetAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Sdl.Web.Mvc.Configuration;
+using Sdl.Web.Modules.SmartTarget.Models;
+
+namespace Sdl.Web.Modules.SmartTarget
+{
+    public class SmartTargetAreaRegistration : BaseAreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SmartTarget";
+            }
+        }
+
+        protected override void RegisterAllViewModels()
+        {
+            // SmartTarget Entity Views
+            RegisterViewModel("Example1", typeof(SmartTargetRegion), "Region");
+            RegisterViewModel("Example2", typeof(SmartTargetRegion), "Region");
+
+        }
+    }
+}
