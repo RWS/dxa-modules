6fc8ec6049157493955f2b5470569426ed517828
Bart Koopman <bkoopman@sdl.com>
2015-09-04 14:55:06 +0200

    Adding sources of ECL and Media Manager modules

---
+++ webapp-net/MediaManager/MediaManagerAreaRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Sdl.Web.Modules.MediaManager.Models;
+using Sdl.Web.Mvc.Configuration;
+
+namespace Sdl.Web.Modules.MediaManager
+{
+    public class MediaManagerAreaRegistration : BaseAreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MediaManager";
+            }
+        }
+
+        protected override void RegisterAllViewModels()
+        {
+			RegisterViewModel("MediaManagerVideo", typeof(MediaManagerVideo));
+        }        
+    }
+}
