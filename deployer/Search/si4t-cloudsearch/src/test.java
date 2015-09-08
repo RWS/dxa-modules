71732a52b88486611b2d0ce825bb1d0f0c53b772
rpannekoek <rpannekoek@sdl.com>
2015-09-08 11:42:08 +0200

    TSI-82 Initial commit of SI4T CloudSearch extensions

---
+++ deployer/Search/si4t-cloudsearch/src/test.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.cloudsearchdomain.AmazonCloudSearchDomainClient;
+import com.amazonaws.services.cloudsearchdomain.model.ContentType;
+import com.amazonaws.services.cloudsearchdomain.model.UploadDocumentsRequest;
+import com.amazonaws.services.cloudsearchdomain.model.UploadDocumentsResult;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Region;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.simpledb.AmazonSimpleDB;
+import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
+import com.amazonaws.services.simpledb.model.DomainMetadataRequest;
+import com.amazonaws.services.simpledb.model.DomainMetadataResult;
+import com.amazonaws.services.simpledb.model.ListDomainsRequest;
+import com.amazonaws.services.simpledb.model.ListDomainsResult;
+
+/**
+ * Welcome to your new AWS Java SDK based project!
+ *
+ * This class is meant as a starting point for your console-based application that
+ * makes one or more calls to the AWS services supported by the Java SDK, such as EC2,
+ * SimpleDB, and S3.
+ *
+ * In order to use the services in this sample, you need:
+ *
+ *  - A valid Amazon Web Services account. You can register for AWS at:
+ *       https://aws-portal.amazon.com/gp/aws/developer/registration/index.html
+ *
+ *  - Your account's Access Key ID and Secret Access Key:
+ *       http://aws.amazon.com/security-credentials
+ *
+ *  - A subscription to Amazon EC2. You can sign up for EC2 at:
+ *       http://aws.amazon.com/ec2/
+ *
+ *  - A subscription to Amazon SimpleDB. You can sign up for Simple DB at:
+ *       http://aws.amazon.com/simpledb/
+ *
+ *  - A subscription to Amazon S3. You can sign up for S3 at:
+ *       http://aws.amazon.com/s3/
+ */
+
+public class test {
+
+    /*
+     * WANRNING:
+     *      To avoid accidental leakage of your credentials, DO NOT keep
+     *      the credentials file in your source directory.
+     */
+
+
+    static AmazonCloudSearchDomainClient csd;
+
+    /**
+     * The only information needed to create a client are security credentials
+     * consisting of the AWS Access Key ID and Secret Access Key. All other
+     * configuration, such as the service endpoints, are performed
+     * automatically. Client parameters, such as proxies, can be specified in an
+     * optional ClientConfiguration object when constructing a client.
+     *
+     * @see com.amazonaws.auth.BasicAWSCredentials
+     * @see com.amazonaws.auth.PropertiesCredentials
+     * @see com.amazonaws.ClientConfiguration
+     */
+    private static void init() throws Exception {
+
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (C:\\Users\\Angel\\.aws\\credentials).
+         */
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider("default").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                            "Please make sure that your credentials file is at the correct " +
+                            "location (C:\\Users\\Angel\\.aws\\credentials), and is in valid format.",
+                    e);
+        }
+
+        csd = new AmazonCloudSearchDomainClient(credentials);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("===========================================");
+        System.out.println("Welcome to the AWS Java SDK!");
+        System.out.println("===========================================");
+
+        init();
+
+        csd.setEndpoint("http://doc-test-cloudsearh-hospira-fxcelsw7ea3virjwbn3inntaoe.us-west-1.cloudsearch.amazonaws.com");
+        UploadDocumentsRequest uploadDocumentsRequest = new UploadDocumentsRequest();
+        uploadDocumentsRequest.setContentType(ContentType.Applicationjson);
+        uploadDocumentsRequest.setDocuments(new FileInputStream("D:\\data.json"));
+
+        UploadDocumentsResult results =  csd.uploadDocuments(uploadDocumentsRequest);
+
+        return;
+
+    }
+}
