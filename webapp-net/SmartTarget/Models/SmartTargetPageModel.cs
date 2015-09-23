c5c1abd949ae3d014dc87929a7d112e484a53ba4
Bart Koopman <bkoopman@sdl.com>
2015-09-23 13:58:57 +0200

    Added missing files which got lost in a merge apparently

---
+++ webapp-net/SmartTarget/Models/SmartTargetPageModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.SmartTarget.Models
+{
+    public class SmartTargetPageModel : PageModel
+    {
+        internal SmartTargetPageModel(PageModel pageModel) : base(pageModel.Id)
+        {
+            Title = pageModel.Title;
+            Url = pageModel.Url;
+
+            MvcData = pageModel.MvcData;
+            HtmlClasses = pageModel.HtmlClasses;
+            Regions.UnionWith(pageModel.Regions);
+            XpmMetadata = pageModel.XpmMetadata;
+
+            foreach (KeyValuePair<string, string> metaKeyValuePair in pageModel.Meta)
+            {
+                Meta.Add(metaKeyValuePair);
+            }
+        }
+        
+        public bool AllowDuplicates { get; set; }
+    }
+}
