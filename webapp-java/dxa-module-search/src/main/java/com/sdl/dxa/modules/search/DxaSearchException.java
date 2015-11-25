d9738c0cfdade4ea6ce6dd986c6f3563d37d7154
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-11-25 15:09:26 +0100

    TSI-1112 exception for search module

---
+++ webapp-java/dxa-module-search/src/main/java/com/sdl/dxa/modules/search/DxaSearchException.java
@@ -0,0 +1,12 @@
+package com.sdl.dxa.modules.search;
+
+public class DxaSearchException extends Exception {
+
+    public DxaSearchException(String message) {
+        super(message);
+    }
+
+    public DxaSearchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
