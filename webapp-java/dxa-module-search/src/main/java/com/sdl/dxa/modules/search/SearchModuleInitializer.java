96f67790dbde377c7c7abead54ffe15a331d1085
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2015-11-25 15:17:20 +0100

    TSI-1112 views mapping

--- webapp-java/dxa-module-search/src/main/java/com/sdl/dxa/modules/search/SearchModuleInitializer.java
+++ webapp-java/dxa-module-search/src/main/java/com/sdl/dxa/modules/search/SearchModuleInitializer.java
@@ -1,12 +1,19 @@
 package com.sdl.dxa.modules.search;
 
+import com.sdl.dxa.modules.search.model.SearchBox;
+import com.sdl.dxa.modules.search.model.SearchItem;
+import com.sdl.dxa.modules.search.model.SearchQuery;
 import com.sdl.webapp.common.api.mapping.views.AbstractInitializer;
 import com.sdl.webapp.common.api.mapping.views.RegisteredView;
-import com.sdl.webapp.common.api.model.entity.Link;
+import com.sdl.webapp.common.api.mapping.views.RegisteredViews;
 import org.springframework.stereotype.Component;
 
 @Component
-@RegisteredView(viewName = "SearchBox", clazz = Link.class)
+@RegisteredViews({
+        @RegisteredView(viewName = "SearchBox", clazz = SearchBox.class),
+        @RegisteredView(viewName = "SearchItem", clazz = SearchItem.class),
+        @RegisteredView(viewName = "SearchResults", clazz = SearchQuery.class, controllerName = "Search")
+})
 public class SearchModuleInitializer extends AbstractInitializer {
 
     @Override
