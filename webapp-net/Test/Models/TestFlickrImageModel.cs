01224c545c25bb652128250f8af0e097cadf2c06
Andrii Dosenko <adosenko@sdl.com>
2015-12-28 16:27:28 +0200

    added TestFlickrImage view and view model for Test module

---
+++ webapp-net/Test/Models/TestFlickrImageModel.cs
@@ -0,0 +1,15 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TestFlickrImageModel", Prefix = "test", Public = true)]
+    public class TestFlickrImageModel : EclItem
+    {
+        [SemanticProperty("test:testProp1")]
+        public string TestProperty1
+        {
+            get;
+            set;
+        }
+    }
+}
