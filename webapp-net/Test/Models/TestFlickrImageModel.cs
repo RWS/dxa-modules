39df19bbbe9664f816612b54717637a45f175ffb
Andrii Dosenko <adosenko@sdl.com>
2016-01-04 12:02:45 +0200

    fix for Test Module Flickr provider

--- webapp-net/Test/Models/TestFlickrImageModel.cs
+++ webapp-net/Test/Models/TestFlickrImageModel.cs
@@ -1,4 +1,5 @@
 ﻿using Sdl.Web.Common.Models;
+using System.Globalization;
 
 namespace Sdl.Web.Modules.Test.Models
 {
@@ -11,5 +12,15 @@ namespace Sdl.Web.Modules.Test.Models
             get;
             set;
         }
+
+        public override string ToHtml(string widthFactor, double aspect = 0, string cssClass = null, int containerSize = 0)
+        {
+            string classAttr = string.IsNullOrEmpty(cssClass) ? string.Empty : string.Format(" class=\"{0}\"", cssClass);
+            string widthAttr = string.IsNullOrEmpty(widthFactor) ? string.Empty : string.Format(" width=\"{0}\"", widthFactor);
+            string aspectAttr = (aspect == 0) ? string.Empty : string.Format(" data-aspect=\"{0}\"", aspect.ToString(CultureInfo.InvariantCulture));
+            return string.Format("<img src=\"{0}\"{1}{2}{3}>", Url, widthAttr, aspectAttr, classAttr);
+        }
     }
+
+
 }
