7ee87e7d5f4d0a60de4c6fc3b84f1ac1a96abd7d
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-01-29 12:48:25 +0100

    TSI-1162 generation of CID urls for Web8

--- webapp-java/dxa-module-cid/src/test/java/com/sdl/webapp/cid/ContextualMediaHelperTest.java
+++ webapp-java/dxa-module-cid/src/test/java/com/sdl/webapp/cid/ContextualMediaHelperTest.java
@@ -3,6 +3,7 @@ package com.sdl.webapp.cid;
 import com.sdl.webapp.common.api.MediaHelper;
 import com.sdl.webapp.common.api.WebRequestContext;
 import com.sdl.webapp.common.api.contextengine.ContextClaimsProvider;
+import com.sdl.webapp.common.impl.DefaultMediaHelper;
 import com.sdl.webapp.common.impl.WebRequestContextImpl;
 import com.sdl.webapp.common.impl.contextengine.ContextEngineImpl;
 import org.junit.Test;
@@ -20,9 +21,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
-/**
- * Unit tests for {@code ContextualMediaHelper}.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ContextualMediaHelperTest.ContextualMediaHelperTestConfig.class)
 public class ContextualMediaHelperTest {
@@ -32,20 +30,20 @@ public class ContextualMediaHelperTest {
 
     @Test
     public void testGetResponsiveImageUrl() throws Exception {
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 12), is("/cid/scale/2048x621/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 6), is("/cid/scale/1024x311/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 2.0, 12), is("/cid/scale/2048x1024/source/site/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 12), is("testCid/cid/scale/2048x621/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 6), is("testCid/cid/scale/1024x311/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 2.0, 12), is("testCid/cid/scale/2048x1024/test/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 12), is("/cid/scale/1024x311/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 6), is("/cid/scale/640x194/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "75%", 2.0, 12), is("/cid/scale/2048x1024/source/site/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 12), is("testCid/cid/scale/1024x311/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 6), is("testCid/cid/scale/640x194/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "75%", 2.0, 12), is("testCid/cid/scale/2048x1024/test/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "320", 2.5, 12), is("/cid/scale/320x128/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "321", 2.5, 12), is("/cid/scale/640x256/source/site/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "320", 2.5, 12), is("testCid/cid/scale/320x128/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "321", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "639", 2.5, 12), is("/cid/scale/640x256/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "640", 2.5, 12), is("/cid/scale/640x256/source/site/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "641", 2.5, 12), is("/cid/scale/1024x410/source/site/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "639", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "640", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "641", 2.5, 12), is("testCid/cid/scale/1024x410/test/example.jpg"));
     }
 
     /**
@@ -66,32 +64,56 @@ public class ContextualMediaHelperTest {
 
         @Bean
         public MediaHelper mediaHelper() {
-            return new ContextualMediaHelper();
+            return new DefaultMediaHelper();
+        }
+
+        @Bean
+        public MediaHelper.ResponsiveMediaUrlBuilder responsiveMediaUrlBuilder() {
+            return new CidResponsiveMediaUrlBuilder();
+        }
+
+        @Bean
+        public MediaHelper.ResponsiveMediaUrlBuilder.HostsNamesProvider hostsNamesProvider() {
+            return new MockHostsNamesProvider();
         }
 
         @Bean
         public WebRequestContext webRequestContext() {
-            return new WebRequestContextImpl() {
-                @Override
-                public int getDisplayWidth() {
-                    return 1920;
-                }
-
-                @Override
-                public double getPixelRatio() {
-                    return 1.0;
-                }
-
-                @Override
-                public int getMaxMediaWidth() {
-                    return 2048;
-                }
-            };
+            return new MockRequestContextImpl();
         }
 
         @Bean
         public HttpServletRequest servletRequest() {
             return new MockHttpServletRequest();
+        }
+
+        private static class MockRequestContextImpl extends WebRequestContextImpl {
+            @Override
+            public int getDisplayWidth() {
+                return 1920;
+            }
+
+            @Override
+            public double getPixelRatio() {
+                return 1.0;
+            }
+
+            @Override
+            public int getMaxMediaWidth() {
+                return 2048;
+            }
+        }
+
+        private static class MockHostsNamesProvider implements MediaHelper.ResponsiveMediaUrlBuilder.HostsNamesProvider {
+            @Override
+            public String getHostname() {
+                return "test";
+            }
+
+            @Override
+            public String getCidHostname() {
+                return "testCid";
+            }
         }
     }
 }
