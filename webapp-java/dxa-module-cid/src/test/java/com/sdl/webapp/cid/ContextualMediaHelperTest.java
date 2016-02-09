f48eddd8090f8b9c79f5d3332e3aff0bae253bca
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-02-09 11:39:12 +0100

    TSI-1271 cid url generation fixed for 2013sp1

--- webapp-java/dxa-module-cid/src/test/java/com/sdl/webapp/cid/ContextualMediaHelperTest.java
+++ webapp-java/dxa-module-cid/src/test/java/com/sdl/webapp/cid/ContextualMediaHelperTest.java
@@ -15,11 +15,16 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ContextualMediaHelperTest.ContextualMediaHelperTestConfig.class)
@@ -28,22 +33,30 @@ public class ContextualMediaHelperTest {
     @Autowired
     private MediaHelper mediaHelper;
 
+    @Autowired
+    private HttpServletRequest servletRequest;
+
     @Test
     public void testGetResponsiveImageUrl() throws Exception {
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 12), is("testCid/cid/scale/2048x621/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 6), is("testCid/cid/scale/1024x311/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 2.0, 12), is("testCid/cid/scale/2048x1024/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 12), is("testCid/cid/scale/2048x621/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 3.3, 6), is("testCid/cid/scale/1024x311/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "100%", 2.0, 12), is("testCid/cid/scale/2048x1024/" + getHostname() + "/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 12), is("testCid/cid/scale/1024x311/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 6), is("testCid/cid/scale/640x194/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "75%", 2.0, 12), is("testCid/cid/scale/2048x1024/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 12), is("testCid/cid/scale/1024x311/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "50%", 3.3, 6), is("testCid/cid/scale/640x194/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "75%", 2.0, 12), is("testCid/cid/scale/2048x1024/" + getHostname() + "/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "320", 2.5, 12), is("testCid/cid/scale/320x128/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "321", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "320", 2.5, 12), is("testCid/cid/scale/320x128/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "321", 2.5, 12), is("testCid/cid/scale/640x256/" + getHostname() + "/example.jpg"));
 
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "639", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "640", 2.5, 12), is("testCid/cid/scale/640x256/test/example.jpg"));
-        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "641", 2.5, 12), is("testCid/cid/scale/1024x410/test/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "639", 2.5, 12), is("testCid/cid/scale/640x256/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "640", 2.5, 12), is("testCid/cid/scale/640x256/" + getHostname() + "/example.jpg"));
+        assertThat(mediaHelper.getResponsiveImageUrl("/example.jpg", "641", 2.5, 12), is("testCid/cid/scale/1024x410/" + getHostname() + "/example.jpg"));
+    }
+
+
+    private String getHostname() throws UnknownHostException {
+        return InetAddress.getLocalHost().getCanonicalHostName() + ':' + servletRequest.getServerPort();
     }
 
     /**
@@ -73,18 +86,16 @@ public class ContextualMediaHelperTest {
         }
 
         @Bean
-        public MediaHelper.ResponsiveMediaUrlBuilder.HostsNamesProvider hostsNamesProvider() {
-            return new MockHostsNamesProvider();
-        }
-
-        @Bean
         public WebRequestContext webRequestContext() {
             return new MockRequestContextImpl();
         }
 
         @Bean
         public HttpServletRequest servletRequest() {
-            return new MockHttpServletRequest();
+            ServletContext servletContext = mock(ServletContext.class);
+            when(servletContext.getInitParameter(eq("cidUrl"))).thenReturn("testCid/cid");
+
+            return new MockHttpServletRequest(servletContext);
         }
 
         private static class MockRequestContextImpl extends WebRequestContextImpl {
@@ -101,18 +112,6 @@ public class ContextualMediaHelperTest {
             @Override
             public int getMaxMediaWidth() {
                 return 2048;
-            }
-        }
-
-        private static class MockHostsNamesProvider implements MediaHelper.ResponsiveMediaUrlBuilder.HostsNamesProvider {
-            @Override
-            public String getHostname() {
-                return "test";
-            }
-
-            @Override
-            public String getCidHostname() {
-                return "testCid/cid";
             }
         }
     }
