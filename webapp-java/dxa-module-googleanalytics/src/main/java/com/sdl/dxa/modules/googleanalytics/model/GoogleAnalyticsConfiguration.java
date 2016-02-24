732a7505677dbefba1735df2e5320784a1c950ba
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-02-24 15:35:54 +0100

    TSI-1323 separate java projects, and remove global parents

--- webapp-java/dxa-module-googleanalytics/src/main/java/com/sdl/dxa/modules/googleanalytics/model/GoogleAnalyticsConfiguration.java
+++ webapp-java/dxa-module-googleanalytics/src/main/java/com/sdl/dxa/modules/googleanalytics/model/GoogleAnalyticsConfiguration.java
@@ -3,15 +3,11 @@ package com.sdl.dxa.modules.googleanalytics.model;
 import com.sdl.webapp.common.api.mapping.semantic.annotations.SemanticEntity;
 import com.sdl.webapp.common.api.mapping.semantic.annotations.SemanticProperty;
 import com.sdl.webapp.common.api.model.entity.AbstractEntityModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.sdl.webapp.common.api.mapping.semantic.config.SemanticVocabulary.SCHEMA_ORG;
 
 @SemanticEntity(entityName = "GoogleAnalyticsConfiguration", vocabulary = SCHEMA_ORG, prefix = "s", public_ = true)
 public class GoogleAnalyticsConfiguration extends AbstractEntityModel {
-
-    private static final Logger LOG = LoggerFactory.getLogger(GoogleAnalyticsConfiguration.class);
 
     @SemanticProperty("siteKey")
     private String siteKey;
