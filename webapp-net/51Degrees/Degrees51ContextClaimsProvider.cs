bebc16c565cb536e39260223bef5e294298f910c
Paul Adams <padams@sdl.com>
2016-04-25 13:03:02 +0100

    TSI-1355 : download lite on initial load

--- webapp-net/51Degrees/Degrees51ContextClaimsProvider.cs
+++ webapp-net/51Degrees/Degrees51ContextClaimsProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Net;
 using System.Web;
 using System.Web.Configuration;
 using FiftyOne.Foundation.Mobile.Detection;
@@ -18,6 +19,8 @@ namespace Sdl.Web.Modules.Degrees51
 {
     public class Degrees51ContextClaimsProvider : IContextClaimsProvider
     {       
+        public static readonly string LITE_URI = "https://github.com/51Degrees/dotNET-Device-Detection/blob/master/data/51Degrees-LiteV3.2.dat?raw=true";
+
         private IAspectMap[] _properties;
         public Degrees51ContextClaimsProvider()
         {        
@@ -124,10 +127,53 @@ namespace Sdl.Web.Modules.Degrees51
                new AspectMap<string> { Aspect = "device", Name = "variant", Build = ()=>GetProperty<string>("DeviceType") },
                new AspectMap<string> { Aspect = "device", Name = "model", Build = ()=>GetProperty<string>("BrowserName") }
             };
+
+            CheckForInitialDownload();
+        }
+
+        private void CheckForInitialDownload()
+        {
+            try
+            {
+                string liteUri = WebConfigurationManager.AppSettings["fiftyOneDegrees.lite.dataset"] ?? LITE_URI;
+                // we need to read the BinaryFilePath to find out where 51degree's is looking for its dataset but unfortunatly this is an
+                // internal property so we use reflection to get it.
+                var configSection = WebConfigurationManager.GetWebApplicationSection("fiftyOne/detection");
+                Type configSectionType = configSection.GetType();
+                System.Reflection.PropertyInfo info = configSectionType.GetProperty("BinaryFilePath", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+                string path = (string)info.GetValue(configSection);
+                if(path.Contains("~"))
+                {
+                    // remap to physical path if required
+                    path = HttpContext.Current.Server.MapPath(path);                                      
+                }
+                FileInfo fileInfo = new FileInfo(path);
+
+                // check if dataset file exists
+                if (!fileInfo.Exists)
+                {
+                    if (!Directory.Exists(fileInfo.DirectoryName))
+                    {
+                        Directory.CreateDirectory(fileInfo.DirectoryName);
+                    }
+                    // we need to download the lite dataset at this point
+                    HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(liteUri);
+                    request.Method = "GET";
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    using (FileStream fileStream = fileInfo.Create())
+                    {
+                        response.GetResponseStream().CopyTo(fileStream);
+                    }                   
+                }                
+            }
+            catch
+            {
+                // ignore this for now as its a just a hack
+            }           
         }
 
         public IDictionary<string, object> GetContextClaims(string aspectName)
-        {
+        {          
             // add license key if available. you can also add the license key to a 
             // file with extension .lic and place it in your /bin folder (i.e. 51Degrees.lic)
             try
@@ -147,6 +193,8 @@ namespace Sdl.Web.Modules.Degrees51
             {
                 Log.Error("An error occured when attempted to access the 51degrees.licenseKey configuration setting.", ex);
             }
+
+            WebProvider.Refresh();
 
             Dictionary<string, object> claims = new Dictionary<string, object>();
             // grab all the properties from the data set and map to context claims                  
@@ -231,7 +279,15 @@ namespace Sdl.Web.Modules.Degrees51
                 {
                     if (Build != null)
                     {
-                        object v = Build();
+                        object v = null;
+                        try
+                        {
+                            Build();
+                        }
+                        catch
+                        {
+                            // ignore
+                        }
                         if (v != null)
                         {                            
                             if (v.GetType() == typeof(string))
@@ -248,6 +304,10 @@ namespace Sdl.Web.Modules.Degrees51
                             }
                         }
                     }
+
+                    if (typeof(T) == typeof(string))
+                        return (T)((object)string.Empty);
+
                     return default(T);
                 }
             }
