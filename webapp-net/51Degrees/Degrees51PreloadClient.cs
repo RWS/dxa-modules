f70b530a6f9ebab69ddcb97a4508b938c1cf6dae
Paul Adams <padams@sdl.com>
2016-05-04 12:11:18 +0100

    TSI-1561 : improved logging

--- webapp-net/51Degrees/Degrees51PreloadClient.cs
+++ webapp-net/51Degrees/Degrees51PreloadClient.cs
@@ -14,6 +14,13 @@ using Sdl.Web.Mvc.Configuration;
 
 namespace Sdl.Web.Modules.Degrees51
 {    
+    /// <summary>
+    /// Degrees51PreloadClient
+    /// 
+    /// This class can be used by IIS with the autoStart settings (if available). If configured correctly
+    /// starting of the site through IIS should check/download the Lite version of the DataSet. If this is
+    /// not configured then the old style Application_Start fallback will be used.
+    /// </summary>
     public class Degrees51PreloadClient : IProcessHostPreloadClient
     {
         public void Preload(string[] parameters)
