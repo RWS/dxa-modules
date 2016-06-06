0dd33c01acc412a936a314967259e8876ea40f27
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-06-06 16:49:43 +0200

    Refactored test-module

--- webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleInitializer.java
+++ webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleInitializer.java
@@ -1,12 +1,19 @@
 package com.sdl.dxa.modules;
 
+import ams.com.sdl.dxa.modules.model.article.Article;
 import com.sdl.dxa.modules.model.ecl.EclTest;
 import com.sdl.webapp.common.api.mapping.views.AbstractInitializer;
 import com.sdl.webapp.common.api.mapping.views.RegisteredViewModel;
+import com.sdl.webapp.common.api.mapping.views.RegisteredViewModels;
 import org.springframework.stereotype.Component;
 
 @Component
-@RegisteredViewModel(viewName = "TestFlickrImage", modelClass = EclTest.class)
+@RegisteredViewModels({
+        // https://jira.sdl.com/browse/TSI-1619
+        @RegisteredViewModel(viewName = "Article", modelClass = Article.class),
+
+        @RegisteredViewModel(viewName = "TestFlickrImage", modelClass = EclTest.class)
+})
 public class TestModuleInitializer extends AbstractInitializer {
 
     @Override
