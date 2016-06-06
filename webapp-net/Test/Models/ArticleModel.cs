c6d9c46c9e02fe4afb17d1bc28f9ae15523c89fb
Andrii Dosenko <adosenko@sdl.com>
2016-06-06 16:01:04 +0300

    TSI-1619 added test model and entity view for .NET version

---
+++ webapp-net/Test/Models/ArticleModel.cs
@@ -0,0 +1,36 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "Article", Prefix = "test", Public = true)]
+    public class ArticleModel : EntityModel
+    {
+        [SemanticProperty("test:heading")]
+        public string Heading
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:media")]
+        public Image Media
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:content")]
+        public RichText Content
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:sideArticle")]
+        public Article SideArticle
+        {
+            get;
+            set;
+        }
+    }
+}
