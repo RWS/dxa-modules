2c57cb895e95eef4365da3fdba7fb5257b42ba15
Rick Pannekoek <rpannekoek@sdl.com>
2016-06-07 14:37:42 +0200

    TSI-789 Factored out DxaDeployUtils.ps1 from ci-deploy.ps1

--- installation/ContextExpressions/ci-deploy.ps1
+++ installation/ContextExpressions/ci-deploy.ps1
@@ -9,59 +9,12 @@ Param(
     [string]$distDestination
 )
 
-function Add-ModelBuilder($modelBuilderType, $configDoc) 
-{
+#Terminate script on first occurred exception
+$ErrorActionPreference = "Stop"
 
-    $modelBuilderPipelineElement = $configDoc.configuration.modelBuilderPipeline
-    $modelBuilderElement = $modelBuilderPipelineElement.SelectSingleNode("add[@type='$modelBuilderType']")
-
-	if ($modelBuilderElement)
-	{
-		Write-Host "Model Builder '$modelBuilderType' is already configured."
-	}
-	else
-	{ 
-        $modelBuilderElement = $configDoc.CreateElement("add")
-		$modelBuilderElement.SetAttribute("type", $modelBuilderType)
-		$modelBuilderPipelineElement.AppendChild($modelBuilderElement) | Out-Null
-		Write-Host "Added Model Builder '$modelBuilderType'."
-    }
-}
-
-function Add-UnityDeclaration($type, $name, $configDoc) {
-	$existing = $configDoc.SelectSingleNode("/unity/*[local-name()='" + $type + "' and @name='" + $name + "']")
-	if ($existing)
-	{
-		Write-Host "$type '$name' is already defined."
-	}
-	else
-	{
-		$aliases = $configDoc.unity.typeAliases
-		$item = $configDoc.CreateElement($type)
-		$item.SetAttribute("name",$name)
-		$configDoc.unity.InsertBefore($item, $aliases) | Out-Null
-	}
-}
-
-function Set-UnityTypeMapping($type, $mapTo, $configDoc) {
-	$mainContainer = $configDoc.unity.containers.container | ? {$_.name -eq "main"}
-	if (!$mainContainer) 
-	{
-        throw "Main container not found."
-    }
-
-	$typeElement = $mainContainer.types.type | ? {$_.type -eq $type}
-	if (!$typeElement)
-	{
-		$typeElement = $configDoc.CreateElement("type")
-		$mainContainer.types.AppendChild($typeElement) | Out-Null
-	}
-
-	$typeElement.SetAttribute("type",$type)
-	$typeElement.SetAttribute("mapTo",$mapTo)
-
-    Write-Host "Set type mapping: '$type' -> '$mapTo'"
-}
+#Include functions from DxaDeployUtil.ps1
+$PSScriptDir = Split-Path $MyInvocation.MyCommand.Path
+. (Join-Path $PSScriptDir "..\DxaDeployUtils.ps1")
 
 $webConfigFile = "$distDestination\Web.config"
 Write-Host "Updating '$webConfigFile' ..."
---
+++ installation/DxaDeployUtils.ps1
@@ -0,0 +1,61 @@
+﻿<#
+.SYNOPSIS
+   Utility functions for DXA.NET Web Application deployment and configuration
+.DESCRIPTION
+This script is intended to be included ("dot-sourced") in another PowerShell script
+#>
+
+
+function Add-ModelBuilder($modelBuilderType, $configDoc) 
+{
+
+    $modelBuilderPipelineElement = $configDoc.configuration.modelBuilderPipeline
+    $modelBuilderElement = $modelBuilderPipelineElement.SelectSingleNode("add[@type='$modelBuilderType']")
+
+	if ($modelBuilderElement)
+	{
+		Write-Host "Model Builder '$modelBuilderType' is already configured."
+	}
+	else
+	{ 
+        $modelBuilderElement = $configDoc.CreateElement("add")
+		$modelBuilderElement.SetAttribute("type", $modelBuilderType)
+		$modelBuilderPipelineElement.AppendChild($modelBuilderElement) | Out-Null
+		Write-Host "Added Model Builder '$modelBuilderType'."
+    }
+}
+
+function Add-UnityDeclaration($type, $name, $configDoc) {
+	$existing = $configDoc.SelectSingleNode("/unity/*[local-name()='" + $type + "' and @name='" + $name + "']")
+	if ($existing)
+	{
+		Write-Host "$type '$name' is already defined."
+	}
+	else
+	{
+		$aliases = $configDoc.unity.typeAliases
+		$item = $configDoc.CreateElement($type)
+		$item.SetAttribute("name",$name)
+		$configDoc.unity.InsertBefore($item, $aliases) | Out-Null
+	}
+}
+
+function Set-UnityTypeMapping($type, $mapTo, $configDoc) {
+	$mainContainer = $configDoc.unity.containers.container | ? {$_.name -eq "main"}
+	if (!$mainContainer) 
+	{
+        throw "Main container not found."
+    }
+
+	$typeElement = $mainContainer.types.type | ? {$_.type -eq $type}
+	if (!$typeElement)
+	{
+		$typeElement = $configDoc.CreateElement("type")
+		$mainContainer.types.AppendChild($typeElement) | Out-Null
+	}
+
+	$typeElement.SetAttribute("type",$type)
+	$typeElement.SetAttribute("mapTo",$mapTo)
+
+    Write-Host "Set type mapping: '$type' -> '$mapTo'"
+}
