386071cc5158984db78a7363984fe1a4fe0ece11
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-06-16 17:43:27 +0200

    TSI-1597 conditional context expressions evaluation

--- webapp-java/dxa-module-context-expressions/src/main/java/com/sdl/dxa/modules/context/builder/ContextExpressionModelBuilder.java
+++ webapp-java/dxa-module-context-expressions/src/main/java/com/sdl/dxa/modules/context/builder/ContextExpressionModelBuilder.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.dd4t.contentmodel.Component;
 import org.dd4t.contentmodel.ComponentPresentation;
 import org.dd4t.contentmodel.Field;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 
@@ -20,26 +21,27 @@ import static com.sdl.webapp.util.dd4t.FieldUtils.getStringValues;
 @Slf4j
 public class ContextExpressionModelBuilder implements EntityBuilder {
 
-    private static final String CONTEXT_EXPRESSIONS_KEY = "ContextExpressions";
+    @Value("${dxa.modules.contextexpr.extension_data_map_key}")
+    private String contextExpressionsKey = "ContextExpressions";
 
     @Override
     public EntityModel createEntity(ComponentPresentation componentPresentation, EntityModel originalEntityModel, Localization localization) throws ContentProviderException {
         log.trace("Context expression model builder for CP {}, entity {} and localization {}", componentPresentation, originalEntityModel, localization);
 
         if (componentPresentation.getExtensionData() == null
-                || !componentPresentation.getExtensionData().containsKey(CONTEXT_EXPRESSIONS_KEY)) {
+                || !componentPresentation.getExtensionData().containsKey(contextExpressionsKey)) {
             log.debug("ContextExpressions not found in {}", componentPresentation);
             return originalEntityModel;
         }
 
-        Map<String, Field> content = componentPresentation.getExtensionData().get(CONTEXT_EXPRESSIONS_KEY).getContent();
+        Map<String, Field> content = componentPresentation.getExtensionData().get(contextExpressionsKey).getContent();
         Conditions conditions = new Conditions(
                 newHashSet(getStringValues(content.get("Include"))),
                 newHashSet(getStringValues(content.get("Exclude"))));
 
         log.debug("Found these context expressions {}", conditions);
 
-        originalEntityModel.addExtensionData(CONTEXT_EXPRESSIONS_KEY, conditions);
+        originalEntityModel.addExtensionData(contextExpressionsKey, conditions);
 
         return originalEntityModel;
     }
--- webapp-java/dxa-module-context-expressions/src/main/java/com/sdl/dxa/modules/context/model/Conditions.java
+++ webapp-java/dxa-module-context-expressions/src/main/java/com/sdl/dxa/modules/context/model/Conditions.java
@@ -10,4 +10,9 @@ public class Conditions {
     private Set<String> includes;
 
     private Set<String> excludes;
+
+    public boolean isEmpty() {
+        return includes == null && excludes == null ||
+                ((includes == null || includes.isEmpty()) && (excludes == null || excludes.isEmpty()));
+    }
 }
