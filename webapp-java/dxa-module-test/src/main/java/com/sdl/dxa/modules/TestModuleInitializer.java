5e47dc7e42b00e1f4b61ba032700c021674c905a
Andrii Dosenko <adosenko@sdl.com>
2016-07-06 17:39:20 +0300

    TSI-1675 test for registering and populating properties from embedded property without associated view for it

--- webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleInitializer.java
+++ webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleInitializer.java
@@ -2,6 +2,8 @@ package com.sdl.dxa.modules;
 
 import ams.com.sdl.dxa.modules.model.article.Article;
 import com.sdl.dxa.modules.model.ecl.EclTest;
+import com.sdl.dxa.modules.model.embed.EmbedChild;
+import com.sdl.dxa.modules.model.embed.EmbedParent;
 import com.sdl.webapp.common.api.mapping.views.AbstractInitializer;
 import com.sdl.webapp.common.api.mapping.views.RegisteredViewModel;
 import com.sdl.webapp.common.api.mapping.views.RegisteredViewModels;
@@ -11,6 +13,10 @@ import org.springframework.stereotype.Component;
 @RegisteredViewModels({
         // https://jira.sdl.com/browse/TSI-1619
         @RegisteredViewModel(viewName = "Article", modelClass = Article.class),
+
+        // https://jira.sdl.com/browse/TSI-1675
+        @RegisteredViewModel(viewName = "EmbedParent", modelClass = EmbedParent.class),
+        @RegisteredViewModel(modelClass = EmbedChild.class),
 
         @RegisteredViewModel(viewName = "TestFlickrImage", modelClass = EclTest.class)
 })
