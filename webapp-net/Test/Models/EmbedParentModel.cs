492bf2134639bce70346e98500a1657ce1ef1872
Andrii Dosenko <adosenko@sdl.com>
2016-07-07 10:19:56 +0300

    TSI-1675 duplicated the same test for .NET version

---
+++ webapp-net/Test/Models/EmbedParentModel.cs
@@ -0,0 +1,22 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "EmbedParent", Prefix = "test", Public = true)]
+    public class EmbedParentModel : EntityModel
+    {
+        [SemanticProperty("test:Title")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:EmbeddedEntity")]
+        public EmbedChildModel EmbeddedEntity
+        {
+            get;
+            set;
+        }
+    }
+}
