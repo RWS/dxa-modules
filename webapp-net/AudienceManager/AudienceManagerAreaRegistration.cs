25651ee881697eb1ff81549bd743f2c2a2126397
Paul Adams <majiccode@gmail.com>
2016-09-09 13:35:57 +0100

    TSI-1922 : initial AM module

---
+++ webapp-net/AudienceManager/AudienceManagerAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Sdl.Web.Modules.AudienceManager.Models;
+using Sdl.Web.Mvc.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Web.Modules.AudienceManager
+{
+    /// <summary>
+    /// AudienceManagerAreaRegistration
+    /// </summary>
+    public class AudienceManagerAreaRegistration : BaseAreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "AudienceManager";
+            }
+        }
+
+        protected override void RegisterAllViewModels()
+        {
+            // Entity Views
+            RegisterViewModel("LoginForm", typeof(LoginForm), "Profile");           
+        }
+    }
+}
---
+++ webapp-net/AudienceManager/Models/LoginForm.cs
@@ -0,0 +1,60 @@
+﻿using Sdl.Web.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sdl.Web.Modules.AudienceManager.Models
+{
+    /// <summary>
+    /// LoginForm entity
+    /// </summary>
+    public class LoginForm : EntityModel
+    {
+        /// <summary>
+        /// Form heading text
+        /// </summary>
+        public string Heading { get; set; }
+
+        /// <summary>
+        /// Holds the form control value for username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Holds the form control value for password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Holds the form control value for remember me
+        /// </summary>
+        public bool RememberMe { get; set; }
+
+        /// <summary>
+        /// Label text for username input control on view
+        /// </summary>
+        public string UserNameLabel { get; set; }
+
+        /// <summary>
+        /// Label text for password input control on view
+        /// </summary>
+        public string PasswordLabel { get; set; }
+
+        /// <summary>
+        /// Label text for remember me check box on view
+        /// </summary>
+        public string RememberMeLabel { get; set; }
+
+        /// <summary>
+        /// Label text for submit botton on view
+        /// </summary>
+        public string SubmitButtonLabel { get; set; }
+
+        /// <summary>
+        /// Text for sucessfull login on view
+        /// </summary>
+        public string LoginSuccessText { get; set; }
+    }
+}
