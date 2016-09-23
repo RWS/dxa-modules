368b442dade8b4697a46d7c035964334ba07d003
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-09-23 16:58:01 +0200

    TSI-1928 redirecting to index from login page

--- webapp-java/dxa-module-audience-manager/src/main/java/com/sdl/dxa/modules/audience/controller/ProfileController.java
+++ webapp-java/dxa-module-audience-manager/src/main/java/com/sdl/dxa/modules/audience/controller/ProfileController.java
@@ -3,13 +3,17 @@ package com.sdl.dxa.modules.audience.controller;
 import com.sdl.dxa.modules.audience.model.LoginForm;
 import com.sdl.dxa.modules.audience.model.validator.LoginFormValidator;
 import com.sdl.webapp.common.api.WebRequestContext;
+import com.sdl.webapp.common.api.model.EntityModel;
 import com.sdl.webapp.common.api.model.ViewModel;
+import com.sdl.webapp.common.api.model.entity.RedirectEntity;
 import com.sdl.webapp.common.controller.ControllerUtils;
 import com.sdl.webapp.common.controller.EntityController;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,26 +31,30 @@ public class ProfileController extends EntityController {
     protected ViewModel enrichModel(ViewModel model, HttpServletRequest request) throws Exception {
 
         if (model instanceof LoginForm) {
-            LoginForm loginForm = (LoginForm) model;
+            final ViewModel enrichedModel = super.enrichModel(model, request);
+            LoginForm loginForm = (LoginForm) (enrichedModel instanceof EntityModel ? enrichedModel : model);
 
-            if ("POST".equals(request.getMethod())) {
-                bindLoginForm(loginForm, request);
-
-                if (!loginForm.getBindingResult().hasErrors()) {
-                    //redirect to index
+            if (modelBindingRequired(model, request)) {
+                loginForm.setBindingResult((BindingResult) request.getAttribute("dataBinding"));
+                if (loginForm.getBindingResult() != null && !loginForm.getBindingResult().hasErrors()) {
+                    return new RedirectEntity(webRequestContext.getLocalization().getPath());
                 }
             }
+
+            return loginForm;
         }
 
-        return super.enrichModel(model, request);
+        return model;
     }
 
-    private void bindLoginForm(@NotNull LoginForm model, @NotNull HttpServletRequest request) {
-        ServletRequestDataBinder dataBinder = new ServletRequestDataBinder(model);
-        dataBinder.bind(request);
-        dataBinder.setValidator(LOGIN_FORM_VALIDATOR);
-        dataBinder.validate();
-        model.setBindingResult(dataBinder.getBindingResult());
+    @Override
+    protected boolean modelBindingRequired(ViewModel model, HttpServletRequest httpServletRequest) {
+        return HttpMethod.POST.name().equals(httpServletRequest.getMethod());
     }
 
+    @Nullable
+    @Override
+    protected Validator dataBindValidator() {
+        return LOGIN_FORM_VALIDATOR;
+    }
 }
