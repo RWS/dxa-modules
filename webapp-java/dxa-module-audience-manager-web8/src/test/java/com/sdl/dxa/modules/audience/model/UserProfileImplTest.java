eaec653e04e595479bb0df7aa03fc2fcd12ac771
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2016-12-14 18:19:47 +0100

    Test for erasing credentials

--- webapp-java/dxa-module-audience-manager-web8/src/test/java/com/sdl/dxa/modules/audience/model/UserProfileImplTest.java
+++ webapp-java/dxa-module-audience-manager-web8/src/test/java/com/sdl/dxa/modules/audience/model/UserProfileImplTest.java
@@ -3,6 +3,7 @@ package com.sdl.dxa.modules.audience.model;
 import com.tridion.marketingsolution.profile.Contact;
 import com.tridion.marketingsolution.utilities.TcmUri;
 import org.junit.Test;
+import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
@@ -10,7 +11,11 @@ import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("Duplicates")
@@ -42,5 +47,19 @@ public class UserProfileImplTest {
 
         //then
         assertNotNull(encoder);
+    }
+
+    @Test
+    public void shouldEraseCredentials() {
+        //given 
+        Contact contact = mock(Contact.class);
+
+        //when
+        UserProfileImpl userProfile = new UserProfileImpl(contact, null, null, "pKey", null);
+        userProfile.eraseCredentials();
+
+        //then
+        verify(contact).setExtendedDetail(eq("pKey"), isNull(String.class));
+        assertTrue(userProfile instanceof CredentialsContainer);
     }
 }
