2bdb43eed5a5cc62768c96766b56920ac22ba620
Orlov Oleksandr <oorlov@sdl.com>
2016-12-20 17:42:01 +0200

    KCWA-247 | Added base tests for Content Navigation

---
+++ gui/test/tests/components/presentation/ContentNavigation.tsx
@@ -0,0 +1,51 @@
+import { ContentNavigation, IContentNavigationItem, IContentNavigationProps } from "components/presentation/ContentNavigation";
+
+// Global Catalina dependencies
+import TestBase = SDL.Client.Test.TestBase;
+
+class ContentNavigationComponent extends TestBase {
+
+    public runTests(): void {
+
+        describe(`ContentNavigation component tests.`, (): void => {
+            const target = super.createTargetElement();
+
+            const navItems: IContentNavigationItem[] = [{
+                url: "/123/456/publication/Title-1",
+                title: "Title-1"
+            }, {
+                url: "/123/567/publication/Title-2",
+                title: "Title-2"
+            }];
+
+            afterEach(() => {
+                const domNode = ReactDOM.findDOMNode(target);
+                ReactDOM.unmountComponentAtNode(domNode);
+            });
+
+            afterAll(() => {
+                target.parentElement.removeChild(target);
+            });
+
+            it("renders content navigation panel", (): void => {
+                this._renderComponent({
+                    navItems: navItems
+                }, target);
+                const element = document.querySelector(".sdl-dita-delivery-content-navigation");
+                expect(element).not.toBeNull();
+                const hyperlinks = element.querySelectorAll("a");
+                expect(hyperlinks.length).toBe(2);
+
+                expect(hyperlinks.item(0).textContent).toBe(navItems[0].title);
+                expect(hyperlinks.item(1).textContent).toBe(navItems[1].title);
+            });
+        });
+
+    }
+
+    private _renderComponent(props: IContentNavigationProps, target: HTMLElement): void {
+        ReactDOM.render(<ContentNavigation {...props} />, target);
+    }
+}
+
+new ContentNavigationComponent().runTests();
