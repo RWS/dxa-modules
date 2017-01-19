3ca133e6751983e09a91295f19e902f252e93672
Andrii Dosenko <adosenko@sdl.com>
2017-01-19 18:54:29 +0200

    TSI-1856 added test view and model to the Test module

---
+++ webapp-net/Test/Models/Tsi1856TestEntity.cs
@@ -0,0 +1,23 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TSI1856", Prefix = "test", Public = true)]
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TSI1856EmbedMeta", Prefix = "m", Public = true)]
+    public class Tsi1856TestEntity : EntityModel
+    {
+        [SemanticProperty("test:title")]
+        public string TitleComponent
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("m:title")]
+        public string TitleMeta
+        {
+            get;
+            set;
+        }
+    }
+}
