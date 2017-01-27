99db9db722bcc2070d5290305a204f4722eaf6e9
Orlov Oleksandr <oorlov@sdl.com>
2017-01-27 15:15:36 +0200

    KCWA-375  | Small tweaks

--- gui/test/tests/Server.tsx
+++ gui/test/tests/Server.tsx
@@ -35,7 +35,7 @@ class Server extends TestBase {
                 };
                 const app = ReactDOMServer.renderToStaticMarkup(<App history={hashHistory} services={services} />);
                 const expected = ReactDOMServer.renderToStaticMarkup((
-                    <div className="sdl-dita-delivery-app">
+                    <div className="sdl-dita-delivery-app sdl-dita-delivery-app-nav">
                         <div className="sdl-dita-delivery-topbar">
                             <div className="sdl-dita-delivery-nav-mask"></div>
                             <header>
