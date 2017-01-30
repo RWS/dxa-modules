c657ebbe8de3215cea843fb252d6a979dc628ab2
rkolupaev <rkolupaev@sdl.com>
2017-01-30 13:44:35 +0100

    KCWA-365 Removed localizationService formatMessage from the component

--- gui/src/components/presentation/ErrorComponent.tsx
+++ gui/src/components/presentation/ErrorComponent.tsx
@@ -3,25 +3,13 @@ import "components/presentation/styles/ErrorComponent";
 import "dist/dita-ot/styles/commonltr";
 import "dist/dita-ot/styles/commonrtl";
 
-import { IAppContext } from "components/container/App";
-
 export interface IErrorComponentProps {
     errorTitle: string;
-    errorMessage: string[];
+    errorMessages: string[];
     errorButtons: JSX.Element;
 }
 
-export interface IPageProps {
-    content?: string | null;
-}
-
 export class ErrorComponent extends React.Component<IErrorComponentProps, {}> {
-
-    public static contextTypes: React.ValidationMap<IAppContext> = {
-        services: React.PropTypes.object.isRequired
-    };
-
-    public context: IAppContext;
 
     constructor(options: IErrorComponentProps) {
         super();
@@ -36,13 +24,12 @@ export class ErrorComponent extends React.Component<IErrorComponentProps, {}> {
      */
     public render(): JSX.Element {
         const props = this.props;
-        const { formatMessage } = this.context.services.localizationService;
-        const message = props.errorMessage.map((value, index) => <p key={index}>{formatMessage(value)}</p>);
+        const messages = props.errorMessages.map((value, index) => <p key={index}>{value}</p>);
 
         return (
             <div className={"error-page-component"}>
-                <h1>{formatMessage(props.errorTitle)}</h1>
-                <div>{message}</div>
+                <h1>{props.errorTitle}</h1>
+                <div>{messages}</div>
                 <div className="sdl-button-group">
                     {props.errorButtons}
                 </div>
