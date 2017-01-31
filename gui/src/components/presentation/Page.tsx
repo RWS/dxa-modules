50f53587abbeae8f12bc58f306fb1339d088997c
Orlov Oleksandr <oorlov@sdl.com>
2017-01-31 18:53:04 +0200

    KCWA-358 | Fixed position, and scrolling

--- gui/src/components/presentation/Page.tsx
+++ gui/src/components/presentation/Page.tsx
@@ -160,7 +160,7 @@ export class Page extends React.Component<IPageProps, IPageState> {
         const activeNavItemId = activeHeader ? activeHeader.id : (navItems.length > 0 ? navItems[0].id : undefined);
 
         return (
-            <div className={"sdl-dita-delivery-page"}>
+            <div className={"sdl-dita-delivery-page"} style={props.showActivityIndicator ? { overflow: 'hidden' } : {}} >
                 {props.showActivityIndicator ? <ActivityIndicator skin="graphene" text={formatMessage("components.app.loading")} /> : null}
                 {props.error ? <ValidationMessage messageType={ValidationMessageType.Error} message={props.error} /> : null}
                 {props.children}
@@ -168,7 +168,7 @@ export class Page extends React.Component<IPageProps, IPageState> {
                 <article>
                     <article className={"page-content ltr"} dangerouslySetInnerHTML={{ __html: props.content || "" }} />
                 </article>
-            </div>
+            </div >
         );
     }
 
