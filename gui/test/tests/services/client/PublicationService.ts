32b3538c77c1ee493e102878d34fef23f25634a8
ofedan <ofedan@sdl.com>
2017-02-01 11:29:15 +0200

    KCWA-471 One more update to the test that I missed last time

--- gui/test/tests/services/client/PublicationService.ts
+++ gui/test/tests/services/client/PublicationService.ts
@@ -55,7 +55,7 @@ class PublicationServiceTests extends TestBase {
                 publicationService.getPublications().then(publications => {
                     expect(publications).toBeDefined();
                     if (publications) {
-                        expect(publications.length).toBe(7);
+                        expect(publications.length).toBe(8);
                         expect(publications[1].title).toBe("Publication MP330");
                         expect(spy).not.toHaveBeenCalled();
                     }
