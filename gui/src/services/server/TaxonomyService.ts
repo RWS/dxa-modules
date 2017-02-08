a3b72be19e016b88fa73d0ff6f29e54a25ad4700
Jan Bevers <jbevers@sdl.com>
2017-02-08 16:33:34 +0100

    KCWA-552 Fixed issue where taxonomy api was called many times for resolving a path

--- gui/src/services/server/TaxonomyService.ts
+++ gui/src/services/server/TaxonomyService.ts
@@ -53,12 +53,12 @@ export class TaxonomyService implements ITaxonomyService {
      * Get the full path for a sitemap item within a sitemap
      *
      * @param {string} publicationId Publication Id
-     * @param {string} sitemapId The sitemap id
+     * @param {string} taxonomyId The taxonomy id (eg t1-k5)
      * @returns {Promise<ITaxonomy[]>} Promise to return the full path
      *
      * @memberOf DataStoreServer
      */
-    public getSitemapPath(publicationId: string, sitemapId: string): Promise<ITaxonomy[]> {
+    public getSitemapPath(publicationId: string, taxonomyId: string): Promise<ITaxonomy[]> {
         return new Promise((resolve: (path?: ITaxonomy[]) => void, reject: (error: string | null) => void) => {
             //
         });
