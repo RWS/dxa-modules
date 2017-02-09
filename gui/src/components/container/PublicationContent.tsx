4c71a664c39345678ade1686b8b1d07cfe86f614
Jan Bevers <jbevers@sdl.com>
2017-02-09 17:09:41 +0100

    KCWA-559 Use the page from the current selected item in the toc to get the breadcrumbs path

--- gui/src/components/container/PublicationContent.tsx
+++ gui/src/components/container/PublicationContent.tsx
@@ -325,7 +325,16 @@ export class PublicationContent extends React.Component<IPublicationContentProps
                     <Breadcrumbs
                         publicationId={publicationId}
                         publicationTitle={publicationTitle || ""}
-                        loadItemsPath={(pubId, taxonomyId) => taxonomyService.getSitemapPath(pubId, pageId || "", taxonomyId)}
+                        loadItemsPath={(pubId, taxonomyId) => {
+                            const currentSelectedTocItem = this.state.selectedTocItem;
+                            if (currentSelectedTocItem) {
+                                const parsedPageUrl = Url.parsePageUrl(currentSelectedTocItem.url || "");
+                                if (parsedPageUrl) {
+                                    return taxonomyService.getSitemapPath(pubId, parsedPageUrl.pageId, taxonomyId);
+                                }
+                            }
+                            return Promise.resolve([]);
+                        } }
                         selectedItem={selectedTocItem}
                         />
                 </Page>
@@ -402,7 +411,7 @@ export class PublicationContent extends React.Component<IPublicationContentProps
             const firstSitemapId = pageInfo.sitemapIds[0];
             const taxonomyId = TcmId.getTaxonomyItemId(TaxonomyItemId.Toc, firstSitemapId) || firstSitemapId;
 
-            this._getActiveSitemapPath(taxonomyId, path => {
+            this._getActiveSitemapPath(pageInfo.id, taxonomyId, path => {
                 /* istanbul ignore if */
                 if (this._isUnmounted) {
                     return;
@@ -432,10 +441,9 @@ export class PublicationContent extends React.Component<IPublicationContentProps
         });
     }
 
-    private _getActiveSitemapPath(sitemapId: string, done: (path: string[]) => void): void {
+    private _getActiveSitemapPath(pageId: string, sitemapId: string, done: (path: string[]) => void): void {
         const { services } = this.context;
-        const { publicationId, pageIdOrPublicationTitle } = this.props.params;
-        const pageId = TcmId.isValidPageId(pageIdOrPublicationTitle) ? pageIdOrPublicationTitle : null;
+        const { publicationId } = this.props.params;
 
         if (pageId) {
             services.taxonomyService.getSitemapPath(publicationId, pageId, sitemapId).then(
