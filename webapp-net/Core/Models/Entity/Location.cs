f83b66d5b57896a6b8f945b555b44393ed9c2c16
Paul Adams <majiccode@gmail.com>
2017-02-10 17:23:22 +0000

    TSI-2284 : fix location

--- webapp-net/Core/Models/Entity/Location.cs
+++ webapp-net/Core/Models/Entity/Location.cs
@@ -4,12 +4,15 @@ using System;
 namespace Sdl.Web.Modules.Core.Models
 {
     [SemanticEntity(Vocab = SchemaOrgVocabulary, EntityName = "GeoCoordinates", Prefix = "s", Public = true)]
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "LocationMeta", Prefix = "l", Public = true)]
     [Serializable]
     public class Location : EntityModel
     {
         [SemanticProperty("s:longitude")]
+        [SemanticProperty("l:longitude")]
         public double Longitude { get; set; }
         [SemanticProperty("s:latitude")]
+        [SemanticProperty("l:latitude")]
         public double Latitude { get; set; }
         public String Query { get; set; } 
     }
