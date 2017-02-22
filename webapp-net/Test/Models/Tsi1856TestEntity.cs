c271484159897a7fcacf2045df17a982fb1207e7
Andrii Dosenko <adosenko@sdl.com>
2017-02-22 10:37:43 +0200

    updated TSI-1856 test view model for .NET

--- webapp-net/Test/Models/Tsi1856TestEntity.cs
+++ webapp-net/Test/Models/Tsi1856TestEntity.cs
@@ -2,11 +2,13 @@
 
 namespace Sdl.Web.Modules.Test.Models
 {
-    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TSI1856", Prefix = "test", Public = true)]
-    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TSI1856EmbedMeta", Prefix = "m", Public = true)]
+    [SemanticEntity("TSI1856")]
+    [SemanticEntity(Vocab = "uuid:d4473ba5-05e0-4320-bfd6-6fbb9eb6265e", EntityName = "TSI1856", Prefix = "test-r")]
+    [SemanticEntity(Vocab = CoreVocabulary, EntityName = "TSI1856EmbedMeta", Prefix = "m")]
     public class Tsi1856TestEntity : EntityModel
     {
-        [SemanticProperty("test:title")]
+        [SemanticProperty("title")]
+        [SemanticProperty("test-r:title")]
         public string TitleComponent
         {
             get;
