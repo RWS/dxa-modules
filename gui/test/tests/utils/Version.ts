39029d3801a9d7dd1e2f52f62f3385301f3a4e6c
Jan Bevers <jbevers@sdl.com>
2017-03-08 15:55:15 +0100

    KCWA-586 Fixed lint error

--- gui/test/tests/utils/Version.ts
+++ gui/test/tests/utils/Version.ts
@@ -3,7 +3,7 @@ import Version from "utils/Version";
 describe(`Version tests.`, (): void => {
 
     it("sorts product release version correctly", (): void => {
-        const sortedProductReleaseVersions = Version.sortProductReleaseVersions([])
+        const sortedProductReleaseVersions = Version.sortProductReleaseVersions([]);
         expect(sortedProductReleaseVersions).toBe([]);
     });
 
