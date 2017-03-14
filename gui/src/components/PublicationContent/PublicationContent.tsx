99065e8e7a4c842938fbb63031acb1f7c4685e41
Maksim <mromanenko@sdl.com>
2017-03-14 01:21:42 +0100

    KCWA-468 Page load

--- gui/src/components/PublicationContent/PublicationContent.tsx
+++ gui/src/components/PublicationContent/PublicationContent.tsx
@@ -2,11 +2,13 @@ import { PublicationContentPresentation } from "./PublicationContentPresentaion"
 import { connect } from "react-redux";
 import { IState } from "store/interfaces/State";
 import { setPublication } from "store/actions/Actions";
-import { getCurrentPub } from "store/reducers/Reducer";
+import { getCurrentPub, getPubById, getPageById } from "store/reducers/Reducer";
 
 const mapStateToProps = (state: IState) => {
     const { publicationId, pageId } = getCurrentPub(state);
-    return { publicationId, pageId };
+    const publication = getPubById(state, publicationId);
+    const page = getPageById(state, pageId)
+    return { publicationId, pageId, publication, page };
 };
 
 const mapDispatchToProps = {
