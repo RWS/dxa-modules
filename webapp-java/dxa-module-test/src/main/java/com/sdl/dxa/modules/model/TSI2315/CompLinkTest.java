c75a7af9c898e36b9a0ed70566a1a3399c247a91
Andrii Dosenko <adosenko@sdl.com>
2017-03-22 00:41:12 +0200

    updated CompLinktest in Java test module

--- webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/model/TSI2315/CompLinkTest.java
+++ webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/model/TSI2315/CompLinkTest.java
@@ -5,10 +5,12 @@ import com.sdl.webapp.common.api.model.EntityModel;
 import com.sdl.webapp.common.api.model.entity.AbstractEntityModel;
 import com.sdl.webapp.common.api.model.entity.Link;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
+@ToString(callSuper = true, exclude = {"compLinkAsEntityModel", "compLinkAsString", "compLinkAsLink"})
 public class CompLinkTest extends AbstractEntityModel {
 
     @SemanticProperty("compLink")
