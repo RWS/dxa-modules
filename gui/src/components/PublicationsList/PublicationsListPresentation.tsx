b37514f4eeda83aa93b4b77c25ac9ce515308361
Maksim <mromanenko@sdl.com>
2017-03-27 08:47:29 +0200

    feature/KCWA-409
    Add default value to VersionSelector

--- gui/src/components/PublicationsList/PublicationsListPresentation.tsx
+++ gui/src/components/PublicationsList/PublicationsListPresentation.tsx
@@ -151,6 +151,8 @@ export class PublicationsListPresentation extends React.Component<IPublicationsL
         const { services, router } = this.context;
         const { formatMessage } = services.localizationService;
         const _retryHandler = (): void => { this.fetchReleaseVersions(this.props); };
+
+        const defaultProductVersion = productReleaseVersions && productReleaseVersions.length ? productReleaseVersions[0].title : "";
         const errorButtons = <div>
             <Button skin="graphene" purpose={ButtonPurpose.CONFIRM} events={{ "click": _retryHandler }}>{formatMessage("control.button.retry")}</Button>
         </div>;
@@ -159,7 +161,7 @@ export class PublicationsListPresentation extends React.Component<IPublicationsL
                 <FetchPublications productFamily={productFamily} />
                 <h1>{productFamily}</h1>
                 <VersionSelector productReleaseVersions={productReleaseVersions}
-                    selectedProductReleaseVersion={productReleaseVersion || "PufPuf"}
+                    selectedProductReleaseVersion={productReleaseVersion || defaultProductVersion}
                     onChange={releaseVersion => {
                         if (router) {
                             router.push(Url.getProductFamilyUrl(productFamily, releaseVersion));
