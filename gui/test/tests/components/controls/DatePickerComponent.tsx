ee3239fb30d9c2559aa885cf8728c9a930d0c959
rkolupaev <rkolupaev@sdl.com>
2017-06-14 11:45:03 +0200

    KCWA-1053 DatePicker test added

---
+++ gui/test/tests/components/controls/DatePickerComponent.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as Moment from "moment";
+import * as TestUtils from "react-addons-test-utils";
+import { DatePicker } from "@sdl/dd/DatePicker/DatePicker";
+import { ComponentWithContext } from "test/mocks/ComponentWithContext";
+import { TestBase } from "@sdl/models";
+
+class DatePickerComponent extends TestBase {
+
+    public runTests(): void {
+
+        describe(`Dropdown component tests.`, (): void => {
+            const target = super.createTargetElement();
+
+            afterEach(() => {
+                const domNode = ReactDOM.findDOMNode(target);
+                ReactDOM.unmountComponentAtNode(domNode);
+            });
+
+            afterAll(() => {
+                if (target.parentElement) {
+                    target.parentElement.removeChild(target);
+                }
+            });
+
+            it("Correct component render", (): void => {
+                const datepicker = this._renderComponent({}, target);
+                const datepickerNode = ReactDOM.findDOMNode(datepicker);
+                const datepickerContainer = datepickerNode.querySelector(".react-datepicker__input-container");
+                const datepickerInput = datepickerNode.querySelector(".react-datepicker__input-container > input");
+                expect(datepickerContainer).not.toBeNull();
+                expect(datepickerInput).not.toBeNull();
+            });
+
+            it("Correct component initial state", (): void => {
+                const datepicker = this._renderComponent({}, target);
+                const datepickerNode = ReactDOM.findDOMNode(datepicker);
+                const datepickerInput = datepickerNode.querySelector("input") as HTMLInputElement;
+                const currentDate = new Date();
+                const moment = Moment(currentDate);
+                expect(datepickerInput.value).toBe(moment.format("MM/DD/YYYY"));
+            });
+
+            it("Toggle component", (): void => {
+                const datepicker = this._renderComponent({}, target);
+                const datepickerNode = ReactDOM.findDOMNode(datepicker);
+                const datepickerInput = datepickerNode.querySelector(".react-datepicker__input-container > input") as HTMLInputElement;
+                TestUtils.Simulate.click(datepickerInput);
+                const datepickerCalendar = document.querySelectorAll(".react-datepicker");
+                expect(datepickerCalendar.length).toBe(1);
+                expect(datepickerCalendar[0]).not.toBeNull();
+            });
+        });
+    }
+
+    private _renderComponent(props: {}, target: HTMLElement): ComponentWithContext {
+        return ReactDOM.render(<ComponentWithContext><DatePicker {...props} /></ComponentWithContext>, target) as ComponentWithContext;
+    }
+}
+
+new DatePickerComponent().runTests();
