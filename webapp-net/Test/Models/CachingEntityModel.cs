3f4c0f81d564dd0bce185ca65f2d2b2c6afd138b
Nataliia Striukova <nstriukova@sdl.com>
2017-06-22 09:46:17 +0300

    Added models and views for cache attribute tests

---
+++ webapp-net/Test/Models/CachingEntityModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    [SemanticEntity(EntityName = "CachingAttributeTest", Prefix = "test", Public = true)]
+    public class CachingEntityModel : EntityModel
+    {
+        [SemanticProperty("test:textField")]
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:numberField")]
+        public int Number
+        {
+            get;
+            set;
+        }
+
+        [SemanticProperty("test:dateField")]
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ webapp-net/Test/Models/CachingPageModel.cs
@@ -0,0 +1,13 @@
+﻿using Sdl.Web.Common.Models;
+
+namespace Sdl.Web.Modules.Test.Models
+{
+    public class CachingPageModel : PageModel
+    {
+        public CachingPageModel(string id)
+                : base(id)
+        {
+        }
+        
+    }
+}
