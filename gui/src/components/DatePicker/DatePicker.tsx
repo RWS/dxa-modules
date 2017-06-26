9c5082f72e634d3c375cf8cdbae9cb998f5af10c
rkolupaev <rkolupaev@sdl.com>
2017-06-26 17:18:06 +0200

    KCWA-1101 Added RTL position for DatePicker Separate Presentation and Connect part

--- gui/src/components/DatePicker/DatePicker.tsx
+++ gui/src/components/DatePicker/DatePicker.tsx
@@ -1,90 +1,19 @@
-import * as React from "react";
 import * as Moment from "moment";
-import DatePickerComponent from "react-datepicker";
 import { connect } from "react-redux";
 import { IState } from "store/interfaces/State";
+import { localization } from "services/common/LocalizationService";
+import { IDatePickerProps, DatePickerPresentation } from "./DatePickerPresentation";
 
-import "react-datepicker/dist/react-datepicker.css";
-import "components/DatePicker/DatePicker.less";
-
-/**
- * DatePicker interface
- *
- * @export
- * @interface IDatePickerProps
- */
-export interface IDatePickerProps {
-    /**
-     * Locale
-     * @type {string}
-     */
-    locale?: string;
-    /**
-     * Default value
-     * @type {string}
-     */
-    value?: string;
-    /**
-     * Change handler
-     */
-    onChangeHandler?: (value: Moment.Moment) => undefined;
-}
-
-/**
- * DatePicker state
- *
- * @export
- * @interface IDatePickerState
- */
-export interface IDatePickerState {
-    selectedDate?: Moment.Moment;
-}
-
-export class DatePicker extends React.Component<IDatePickerProps, IDatePickerState> {
-
-    /**
-     * Creates an instance of DatePicker
-     */
-    constructor() {
-        super();
-        this.state = {
-            selectedDate: undefined
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    /**
-     * Render the component
-     *
-     * @returns {JSX.Element}
-     */
-    public render(): JSX.Element {
-        return (
-            <DatePickerComponent
-                selected={this.state.selectedDate || (this.props.value ? Moment(this.props.value, "YYYYMMDD") : undefined)}
-                onChange={this.handleChange}
-                calendarClassName="sdl-dita-delivery-datepicker"
-                className="sdl-input-text"
-                locale={this.props.locale}
-            />
-        );
-    }
-
-    private handleChange(date: Moment.Moment): void {
-        this.setState({
-            selectedDate: date
-        }, (this.props.onChangeHandler) ? this.props.onChangeHandler(date) : undefined);
-    }
-}
-
-const mapStateToProps = (state: IState): {} => {
+const mapStateToProps = (state: IState, ownProps: IDatePickerProps): {} => {
     // Set minimum weekdays name (2 char length) to its short value (3 char length) for current locale
     Moment.locale(state.language);
     Moment.updateLocale(state.language, {
         weekdaysMin: Moment.weekdaysShort()
     });
     return {
-        locale: state.language
+        locale: state.language,
+        direction: localization.getDirection(state.language),
+        ...ownProps
     };
 };
 
@@ -93,4 +22,4 @@ const mapStateToProps = (state: IState): {} => {
  *
  * @export
  */
-export const LanguageDatePicker = connect(mapStateToProps)(DatePicker);
+export const DatePicker = connect(mapStateToProps)(DatePickerPresentation);
