f173ee48392e31ba85483fb29b864596969a53de
Maksim <mromanenko@sdl.com>
2017-06-29 11:38:00 +0200

    KCWA-1067 Fixes linter

--- gui/src/components/ConditionsDialog/ConditionsDialog.tsx
+++ gui/src/components/ConditionsDialog/ConditionsDialog.tsx
@@ -1,7 +1,7 @@
 import { IState } from "store/interfaces/State";
 import { connect } from "react-redux";
 import { isEmpty } from "lodash";
-import { ConditionsDialogPresentation } from './ConditionsDialogPresentation';
+import { ConditionsDialogPresentation } from "./ConditionsDialogPresentation";
 import { dialogOpen, dialogClose, applyConditions, updateEditingConditions } from "store/actions/Actions";
 import { getCurrentPub, isConditionsDialogVisible, getAllConditionsByPubId, getEditingConditions, getLastConditions } from "store/reducers/Reducer";
 
