5a79190dcf4f83e91cad49ae9e85ad054feddd74
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-08-08 10:10:53 +0200

    Build fix

--- webapp-java/dxa-module-smarttarget/src/test/java/com/sdl/dxa/modules/smarttarget/mapping/SmartTargetPageBuilderTest.java
+++ webapp-java/dxa-module-smarttarget/src/test/java/com/sdl/dxa/modules/smarttarget/mapping/SmartTargetPageBuilderTest.java
@@ -64,7 +64,7 @@ public class SmartTargetPageBuilderTest {
     public void shouldReturnNullIfPageModelIsNull() throws ContentProviderException {
         //when, then
         //noinspection ConstantConditions
-        assertNull(pageBuilder.buildPageModel(null, new PageModelData("", null, null, null, null, null)));
+        assertNull(pageBuilder.buildPageModel(null, new PageModelData("", null, null, null, null, null, null)));
     }
 
     @Test
@@ -75,7 +75,7 @@ public class SmartTargetPageBuilderTest {
         PageModel expected = createPageModel(new RegionModelImpl("test"));
 
         //when
-        PageModel page2 = pageBuilder.buildPageModel(pageModel, new PageModelData("", null, null, null, null, null));
+        PageModel page2 = pageBuilder.buildPageModel(pageModel, new PageModelData("", null, null, null, null, null, null));
 
         //then
         Assert.assertEquals(expected, pageModel);
@@ -97,7 +97,7 @@ public class SmartTargetPageBuilderTest {
         PageModel expected = createPageModel(new SmartTargetRegion("test"));
 
         //when
-        PageModel pageR2 = pageBuilder.buildPageModel(pageModel, new PageModelData("", null, null, null, null, ""));
+        PageModel pageR2 = pageBuilder.buildPageModel(pageModel, new PageModelData("", null, null, null, null, null, ""));
 
         //then
         Assert.assertEquals(expected, pageModel);
@@ -111,7 +111,7 @@ public class SmartTargetPageBuilderTest {
         RegionModelData regionModelData = RegionModelData.builder().name("test").metadata(new ContentModelData() {{
             put("maxItems", maxItemsValue);
         }}).build();
-        PageModelData pageModelData = new PageModelData("id", Collections.emptyMap(), null, "title", Lists.newArrayList(regionModelData), "");
+        PageModelData pageModelData = new PageModelData("id", null, Collections.emptyMap(), null, "title", Lists.newArrayList(regionModelData), "");
 
         SmartTargetPageModel stPageModel = Mockito.mock(SmartTargetPageModel.class);
         Mockito.when(stPageModel.setAllowDuplicates(Matchers.anyBoolean())).thenReturn(stPageModel);
