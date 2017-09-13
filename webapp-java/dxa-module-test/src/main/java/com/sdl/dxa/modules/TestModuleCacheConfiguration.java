1e4d9e5147f719399df61085c513528f29ac319d
Alexey Zarakovskiy <azarakovskiy@sdl.com>
2017-09-13 19:27:34 +0200

    TSI-2647 DXA caching refactoring and using of CIL configuration for it (TSI-2611)

--- webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleCacheConfiguration.java
+++ webapp-java/dxa-module-test/src/main/java/com/sdl/dxa/modules/TestModuleCacheConfiguration.java
@@ -1,5 +1,6 @@
 package com.sdl.dxa.modules;
 
+import com.sdl.dxa.caching.NamedCacheProvider;
 import com.sdl.dxa.caching.NeverCached;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,12 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.annotation.PostConstruct;
-import javax.cache.CacheManager;
 import javax.cache.configuration.FactoryBuilder;
 import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
 import javax.cache.event.CacheEntryCreatedListener;
@@ -20,13 +17,9 @@ import javax.cache.event.CacheEntryEvent;
 import javax.cache.event.CacheEntryEventFilter;
 import javax.cache.event.CacheEntryListenerException;
 import javax.cache.event.EventType;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.http.HTTPException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
 
 @Configuration
 @Profile("!dxa.no-cache")
@@ -35,7 +28,7 @@ public class TestModuleCacheConfiguration {
     private static final Logger log = LoggerFactory.getLogger(TestModuleCacheConfiguration.class);
 
     @Autowired
-    private CacheManager cacheManager;
+    private NamedCacheProvider cacheProvider;
 
     @PostConstruct
     public void configure() {
@@ -44,8 +37,8 @@ public class TestModuleCacheConfiguration {
                         FactoryBuilder.factoryOf(new NeverCacheListener<>()),
                         FactoryBuilder.factoryOf(new OnlyCreatedFilter<>()),
                         false, true);
-        cacheManager.getCacheNames()
-                .forEach(cacheName -> cacheManager.getCache(cacheName).registerCacheEntryListener(listenerConfiguration));
+        cacheProvider.getCacheManager().getCacheNames()
+                .forEach(cacheName -> cacheProvider.getCache(cacheName).registerCacheEntryListener(listenerConfiguration));
     }
 
     private static class NeverCacheListener<K, V> implements CacheEntryCreatedListener<K, V>, Serializable {
