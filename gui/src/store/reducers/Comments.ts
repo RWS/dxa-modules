4734d7eafdb942a00405b61ecc0dba0554e5dab2
Orlov Oleksandr <oorlov@sdl.com>
2017-09-29 09:20:56 +0300

    SCTCD-342 | Fix for comment to add into proper place

--- gui/src/store/reducers/Comments.ts
+++ gui/src/store/reducers/Comments.ts
@@ -6,7 +6,7 @@ import { ICommentsPayload, ICommentsError, IComments, ICommentPayload, ICommentE
 export interface ICommentsErrorPayload {
     key: string;
     error: ICommentsError;
-};
+}
 
 const removeByKey = (myObj: ICommentErrorsMap, deleteKey: string) => {
     return Object.keys(myObj)
@@ -18,24 +18,40 @@ const removeByKey = (myObj: ICommentErrorsMap, deleteKey: string) => {
 };
 
 const byId = combine(
-    handleAction(COMMENTS_LOADED, (state, { comments, key }) => {
-        return ({ ...state, [key]: comments });
-    }, {}),
-    handleAction(COMMENT_SAVED, (state, { comment, key }) => {
-        return ({ ...state, [key]: state[key].concat(comment).sort((a: IComment, b: IComment) => b.id - a.id) });
-    }, {})
+    handleAction(
+        COMMENTS_LOADED,
+        (state, { comments, key }) => {
+            return { ...state, [key]: comments };
+        },
+        {}
+    ),
+    handleAction(
+        COMMENT_SAVED,
+        (state, { comment, key }) => {
+            const stateComments = state[key] as IComment[];
+            const savedCommentParentId = (comment as IComment).parentId;
+            const parentItem = stateComments.find((c: IComment) => c.id == savedCommentParentId) as IComment;
+            if (parentItem) {
+                parentItem.children = parentItem.children.concat(comment).sort((a: IComment, b: IComment) => b.id - a.id);
+                return { ...state, [key]: stateComments };
+            } else {
+                return { ...state, [key]: stateComments.concat(comment).sort((a: IComment, b: IComment) => b.id - a.id) };
+            }
+        },
+        {}
+    )
 );
 
 const loading = combine(
     handleAction(COMMENTS_LOADING, (state: string[], key: string) => [...state, key], []),
-    handleAction(COMMENTS_LOADED, (state: string[], { key }: ICommentsPayload) => state.filter((k) => key !== k), []),
-    handleAction(COMMENTS_ERROR, (state: string[], { key, error }: ICommentsErrorPayload) => state.filter((k) => key !== k), [])
+    handleAction(COMMENTS_LOADED, (state: string[], { key }: ICommentsPayload) => state.filter(k => key !== k), []),
+    handleAction(COMMENTS_ERROR, (state: string[], { key, error }: ICommentsErrorPayload) => state.filter(k => key !== k), [])
 );
 
 const saving = combine(
     handleAction(COMMENT_SAVING, (state: string[], key: string) => [...state, key], []),
-    handleAction(COMMENT_SAVED, (state: string[], { key }: ICommentPayload) => state.filter((k) => key !== k), []),
-    handleAction(COMMENT_ERROR, (state: string[], { key }: ICommentErrorsMap) => state.filter((k) => key !== k), [])
+    handleAction(COMMENT_SAVED, (state: string[], { key }: ICommentPayload) => state.filter(k => key !== k), []),
+    handleAction(COMMENT_ERROR, (state: string[], { key }: ICommentErrorsMap) => state.filter(k => key !== k), [])
 );
 
 const errors = combine(
@@ -57,7 +73,7 @@ export const comments = combineReducers({
     postErrors
 });
 
-export const getById = (state: IComments, id: string) => (id in state.byId) ? state.byId[id] : ([] as IComment[]);
-export const getErrorMessage = (state: IComments, id: string): string => id in state.errors ? state.errors[id] : "";
-export const getPostErrorMessage = (state: IComments, id: string): string => id in state.postErrors ? state.postErrors[id] : "";
+export const getById = (state: IComments, id: string) => (id in state.byId ? state.byId[id] : [] as IComment[]);
+export const getErrorMessage = (state: IComments, id: string): string => (id in state.errors ? state.errors[id] : "");
+export const getPostErrorMessage = (state: IComments, id: string): string => (id in state.postErrors ? state.postErrors[id] : "");
 export const isSaving = (state: IComments, id: string): boolean => state.saving.includes(id);
