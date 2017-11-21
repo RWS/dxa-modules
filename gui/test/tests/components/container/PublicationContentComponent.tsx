5cd1b4227b5e013cc6d69f2c5257a4716c344287
Igor Nikonov <inikonov@sdl.com>
2017-11-21 17:13:18 +0200

    SCTCD-594 changed test to scry instead of find. Find expect at least one, scry does not expect any

--- gui/test/tests/components/container/PublicationContentComponent.tsx
+++ gui/test/tests/components/container/PublicationContentComponent.tsx
@@ -293,8 +293,7 @@ class PublicationContentComponent extends TestBase {
 
                 // Use a timeout to allow the DataStore to return a promise with the data
                 setTimeout((): void => {
-                    const dropdownList = TestUtils.findRenderedComponentWithType(publicationContent, DropdownList as any);
-                    expect(dropdownList).toBeUndefined();
+                    expect(TestUtils.scryRenderedComponentsWithType(publicationContent, DropdownList as any).length).toBe(0, "Version selector should not be rendered.");
                     done();
                 }, RENDER_DELAY);
             });
