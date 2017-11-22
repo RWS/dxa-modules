993475f5a64e6d3509fb40527897dd466763acaa
Oleksandr Orlov <oorlov@sdl.com>
2017-11-22 20:32:14 +0200

    Revert "SCTCD-594 changed test to scry instead of find. Find expect at least one, scry does not expect any"
    This reverts commit 5aca19c4255f312a42041d6e2dd09f6e5d206d88.

--- gui/test/tests/components/container/PublicationContentComponent.tsx
+++ gui/test/tests/components/container/PublicationContentComponent.tsx
@@ -293,7 +293,8 @@ class PublicationContentComponent extends TestBase {
 
                 // Use a timeout to allow the DataStore to return a promise with the data
                 setTimeout((): void => {
-                    expect(TestUtils.scryRenderedComponentsWithType(publicationContent, DropdownList as any).length).toBe(0, "Version selector should not be rendered.");
+                    const dropdownList = TestUtils.findRenderedComponentWithType(publicationContent, DropdownList as any);
+                    expect(dropdownList).toBeUndefined();
                     done();
                 }, RENDER_DELAY);
             });
