9b438c7eec48233aa75497277a3a65c6c5d2a57b
Oleksandr Orlov <oorlov@sdl.com>
2017-12-18 12:29:16 +0200

    SCTCD-682 | Fixed test

--- gui/test/tests/components/presentation/NavigationMenuComponent.tsx
+++ gui/test/tests/components/presentation/NavigationMenuComponent.tsx
@@ -3,14 +3,13 @@ import * as ReactDOM from "react-dom";
 import { NavigationMenu, INavigationMenuProps } from "@sdl/dd/presentation/NavigationMenu";
 import { TestBase } from "@sdl/models";
 
-import { RENDER_DELAY } from "test/Constants";
-
 class NavigationMenuComponent extends TestBase {
-
     public runTests(): void {
-
         describe(`Navigation Menu tests`, (): void => {
             const target = super.createTargetElement();
+            const defaultProps = {
+                isOpen: false
+            };
 
             afterEach(() => {
                 const domNode = ReactDOM.findDOMNode(target);
@@ -24,40 +23,29 @@ class NavigationMenuComponent extends TestBase {
             });
 
             it("renders", (): void => {
-                this._renderComponent({
-                    isOpen: false
-                }, target);
+                this._renderComponent(defaultProps, target);
                 const element = document.querySelector(".sdl-dita-delivery-navigation-menu");
                 expect(element).not.toBeNull();
                 expect((element as HTMLElement).innerHTML).toBe("<div>Contents</div>");
             });
 
-            it("can open/close", (done: () => void): void => {
-                this._renderComponent({
-                    isOpen: false
-                }, target);
+            it("can open/close", (): void => {
+                this._renderComponent(defaultProps, target);
                 const element = document.querySelector(".sdl-dita-delivery-navigation-menu") as HTMLElement;
                 expect(element.classList).not.toContain("open");
-                expect(getComputedStyle(element).left).toBe("-300px");
-                this._renderComponent({
-                    isOpen: true
-                }, target);
+                this._renderComponent({ ...defaultProps, isOpen: true }, target);
                 expect(element.classList).toContain("open");
-                // Animation takes .3s, use a slightly bigger timeout
-                setTimeout((): void => {
-                    const left = getComputedStyle(element).left;
-                    expect(left).not.toBeNull();
-                    if (left) {
-                        expect(parseInt(left, 10)).toBe(0);
-                    }
-                    done();
-                }, RENDER_DELAY);
             });
         });
     }
 
     private _renderComponent(props: INavigationMenuProps, target: HTMLElement): void {
-        ReactDOM.render(<NavigationMenu {...props}><div>Contents</div></NavigationMenu>, target);
+        ReactDOM.render(
+            <NavigationMenu {...props}>
+                <div>Contents</div>
+            </NavigationMenu>,
+            target
+        );
     }
 }
 
