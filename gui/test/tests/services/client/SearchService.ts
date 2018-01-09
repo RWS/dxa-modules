4687120dc74929e79b07d0e92e4b901f7d86f54a
Oleksandr Orlov <oorlov@sdl.com>
2018-01-09 12:50:13 +0200

    SCTCD-659 | Added search error message coverage

--- gui/test/tests/services/client/SearchService.ts
+++ gui/test/tests/services/client/SearchService.ts
@@ -2,16 +2,25 @@ import { SearchService } from "services/client/SearchService";
 import { TestBase } from "@sdl/models";
 import { IWindow } from "interfaces/Window";
 import { ISearchQuery } from "interfaces/Search";
+import { FakeXMLHttpRequest } from "test/mocks/XmlHttpRequest";
 
+interface IXMLHttpRequestWindow extends Window {
+    XMLHttpRequest: {};
+}
 class PageServiceTests extends TestBase {
-
     public runTests(): void {
-        const win = (window as IWindow);
+        const win = window as IWindow;
         const mocksFlag = win.SdlDitaDeliveryMocksEnabled;
         const searchService = new SearchService();
         const startIndex = 0;
         const _testQuery = "SDL: WEB";
         const _testLocale = "en";
+
+        const defaultQuery = {
+            startIndex,
+            locale: _testLocale,
+            searchQuery: _testQuery
+        } as ISearchQuery;
 
         describe("Search service tests.", (): void => {
             beforeEach(() => {
@@ -23,43 +32,54 @@ class PageServiceTests extends TestBase {
             });
 
             it("can get search results", (done: () => void): void => {
-                const query = {
-                    startIndex,
-                    locale: _testLocale,
-                    searchQuery: _testQuery
-                } as ISearchQuery;
-                searchService.getSearchResults(query).then(searchResults => {
-                    expect(searchResults).toBeDefined();
-                    if (searchResults) {
-                        expect(searchResults.hits).toBe(148);
-                        expect(searchResults.queryResults.length).toBe(10);
-                    }
-                    done();
-                }).catch(error => {
-                    fail(`Unexpected error: ${error}`);
-                    done();
-                });
+                searchService
+                    .getSearchResults(defaultQuery)
+                    .then(searchResults => {
+                        expect(searchResults).toBeDefined();
+                        if (searchResults) {
+                            expect(searchResults.hits).toBe(148);
+                            expect(searchResults.queryResults.length).toBe(10);
+                        }
+                        done();
+                    })
+                    .catch(error => {
+                        fail(`Unexpected error: ${error}`);
+                        done();
+                    });
             });
 
             it("is not cached in memory", (done: () => void): void => {
-                const query = {
-                    startIndex,
-                    locale: _testLocale,
-                    searchQuery: _testQuery
-                } as ISearchQuery;
                 const spy = spyOn(XMLHttpRequest.prototype, "open").and.callThrough();
-                searchService.getSearchResults(query).then(searchResults => {
-                    expect(searchResults).toBeDefined();
-                    if (searchResults) {
-                        expect(searchResults.hits).toBe(148);
-                        expect(searchResults.queryResults.length).toBe(10);
-                        expect(spy).toHaveBeenCalled();
-                    }
-                    done();
-                }).catch(error => {
-                    fail(`Unexpected error: ${error}`);
-                    done();
-                });
+                searchService
+                    .getSearchResults(defaultQuery)
+                    .then(searchResults => {
+                        expect(searchResults).toBeDefined();
+                        if (searchResults) {
+                            expect(searchResults.hits).toBe(148);
+                            expect(searchResults.queryResults.length).toBe(10);
+                            expect(spy).toHaveBeenCalled();
+                        }
+                        done();
+                    })
+                    .catch(error => {
+                        fail(`Unexpected error: ${error}`);
+                        done();
+                    });
+            });
+
+            it("returns a proper error when search can not happen", (done: () => void): void => {
+                const failMessage = "failure-searchin-item";
+                spyOn(window as IXMLHttpRequestWindow, "XMLHttpRequest").and.callFake(() => new FakeXMLHttpRequest(failMessage));
+                searchService
+                    .getSearchResults(defaultQuery)
+                    .then(searchResults => {
+                        fail("Search should not happen");
+                        done();
+                    })
+                    .catch(error => {
+                        expect(error).toContain(failMessage);
+                        done();
+                    });
             });
         });
     }
