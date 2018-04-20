815ee2717745d4bd65fcab0ad242f6a0b96b6bda
Paul Adams <majiccode@gmail.com>
2018-04-20 12:15:30 +0100

    TSI-3165 : Tweak to model builder

--- webapp-net/Ugc/Mapping/UgcModelBuilder.cs
+++ webapp-net/Ugc/Mapping/UgcModelBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Sdl.Web.Common.Interfaces;
 using Sdl.Web.Common.Logging;
@@ -10,54 +11,107 @@ using Sdl.Web.Tridion.Mapping;
 
 namespace Sdl.Web.Modules.Ugc.Mapping
 {
+    /// <summary>
+    /// Ugc Model Builder
+    /// </summary>
     public class UgcModelBuilder : IPageModelBuilder, IEntityModelBuilder
     {
         private static readonly string ShowCommentsExtData = "UgcShowComments";
         private static readonly string PostCommentsExtData = "UgcPostComments";
+        private static readonly string CommentsEntityRegionExt = "CommentsEntityRegion";
 
         public void BuildPageModel(ref PageModel pageModel, PageModelData pageModelData, bool includePageRegions,
             ILocalization localization)
         {
             using (new Tracer(pageModel, pageModelData, localization))
             {
-                UgcRegion ugcRegion = GetRegion(pageModel);
-
-                foreach (var region in pageModel.Regions)
-                {                    
-                    AddCommentsToRegion(ugcRegion, region, localization);
-                }
-
                 var ugcMetadata = UgcMetadata(pageModelData.PageTemplate.Metadata);
-                if (ShowComments(ugcMetadata))
+
+                string regionName = GetValue<string>(ugcMetadata, "commentsRegion");
+                string areaName = pageModel.MvcData.AreaName;
+                RegionModel ugcRegion;
+                if (string.IsNullOrEmpty(regionName))
                 {
-                    ugcRegion.Entities.Add(CreateUgcCommentsEntity(localization, pageModel.Id, ItemType.Page));
+                    areaName = "Ugc";
+                    regionName = "Comments";
+                    ugcRegion = FindRegion(pageModel.Regions, "Comments") ??
+                                CreateRegion(pageModel, areaName, regionName);
                 }
-                if (PostComments(ugcMetadata))
+                else
                 {
-                    ugcRegion.Entities.Add(CreateUgcPostCommentEntity(localization, pageModel.Id, ItemType.Page, UgcPostFormMetadata(ugcMetadata)));
+                    ugcRegion = FindRegion(pageModel.Regions, regionName);
+                    if (ugcRegion == null)
+                    {
+                        Log.Error("Unable to locate region for comments '" + regionName + "'.");
+                    }
+                }
+
+                // Entity Comments
+                foreach (var region in pageModel.Regions)
+                {
+                    AddCommentsViews(pageModel, region, localization, ugcRegion);
+                }
+
+                if (ugcRegion != null)
+                {
+                    // Page Comments                
+                    if (ShowComments(ugcMetadata))
+                    {
+                        ugcRegion.Entities.Add(CreateUgcCommentsEntity(localization, pageModel.Id, ItemType.Page));
+                    }
+                    if (PostComments(ugcMetadata))
+                    {
+                        ugcRegion.Entities.Add(CreateUgcPostCommentEntity(localization, pageModel.Id, ItemType.Page,
+                            UgcPostFormMetadata(ugcMetadata)));
+                    }
                 }
             }
         }        
 
-        private static void AddCommentsToRegion(UgcRegion ugcRegion, RegionModel region, ILocalization localization)
+        private static void AddCommentsViews(PageModel pageModel, RegionModel region, ILocalization localization, RegionModel ugcRegion)
         {
-            if (ugcRegion == region) return;
+            List<EntityModel> regionEntities = new List<EntityModel>();
+            
             foreach (var entity in region.Entities.Where(e => e.ExtensionData != null))
             {
+                if (entity.ExtensionData == null) continue;
+
+                // comments get added to the ugcRegion if it exists else we place in same region as entity
+                IList<EntityModel> entities = ugcRegion != null ? ugcRegion.Entities : regionEntities;
+                if (entity.ExtensionData.ContainsKey(CommentsEntityRegionExt))
+                {
+                    // comment region specified for this entity so lets find it and use that
+                    var targetRegion = FindRegion(pageModel.Regions, (string) entity.ExtensionData[CommentsEntityRegionExt]);
+                    if (targetRegion != null && targetRegion != region)
+                    {
+                        entities = targetRegion.Entities;
+                    }
+                    else if (targetRegion == null || targetRegion == region)
+                    {
+                        entities = regionEntities;
+                    }
+                }
+
                 if (entity.ExtensionData.ContainsKey(ShowCommentsExtData) &&
                     (bool) entity.ExtensionData[ShowCommentsExtData])
                 {
-                    ugcRegion.Entities.Add(CreateUgcCommentsEntity(localization, entity.Id, ItemType.Component));
+                    entities.Add(CreateUgcCommentsEntity(localization, entity.Id, ItemType.Component));
                 }
                 if (entity.ExtensionData.ContainsKey(PostCommentsExtData) && entity.ExtensionData[PostCommentsExtData] != null)
                 {
-                    ugcRegion.Entities.Add(CreateUgcPostCommentEntity(localization, entity.Id, ItemType.Component, (ContentModelData)entity.ExtensionData[PostCommentsExtData]));
+                    entities.Add(CreateUgcPostCommentEntity(localization, entity.Id, ItemType.Component, (ContentModelData)entity.ExtensionData[PostCommentsExtData]));
                 }
+            }
+
+            // Add our ugc views to either the same region as the entity we have comments enabled for or the ugc "Comments" region if available
+            foreach (var x in regionEntities)
+            {
+                region.Entities.Add(x);
             }
 
             foreach (var childRegion in region.Regions)
             {
-                AddCommentsToRegion(ugcRegion, childRegion, localization);
+                AddCommentsViews(pageModel, childRegion, localization, ugcRegion);
             }
         }
 
@@ -67,15 +121,26 @@ namespace Sdl.Web.Modules.Ugc.Mapping
             var ugcMetadata = UgcMetadata(entityModelData.ComponentTemplate.Metadata);
             entityModel.SetExtensionData(ShowCommentsExtData, ShowComments(ugcMetadata));
             entityModel.SetExtensionData(PostCommentsExtData, PostComments(ugcMetadata) ? UgcPostFormMetadata(ugcMetadata) : null);
+            entityModel.SetExtensionData(CommentsEntityRegionExt, GetCommentsRegion(ugcMetadata));
         }
 
-        private static UgcRegion GetRegion(PageModel pageModel)
+        private static RegionModel FindRegion(RegionModelSet regionModelSet, string regionName)
+        {
+            foreach (var region in regionModelSet)
+            {
+                if (region.Name.Equals(regionName)) return region;
+                RegionModel childRegion = FindRegion(region.Regions, regionName);
+                if (childRegion != null) return childRegion;
+            }
+
+            return null;
+        }
+
+        private static UgcRegion CreateRegion(PageModel pageModel, string areaName, string regionName)
         {
             UgcRegion ugcRegion;
             if (!pageModel.Regions.OfType<UgcRegion>().Any())
             {
-                const string areaName = "Ugc";
-                const string regionName = "Comments";
                 ugcRegion = new UgcRegion(regionName);
                 ugcRegion.MvcData = new Common.Models.MvcData($"{areaName}:{regionName}");
                 pageModel.Regions.Add(ugcRegion);
@@ -121,6 +186,8 @@ namespace Sdl.Web.Modules.Ugc.Mapping
                 IsVolatile = true
             };
         }
+
+        private static string GetCommentsRegion(ContentModelData metadata) => GetValue<string>(metadata, "commentsRegion");
 
         private static ContentModelData UgcMetadata(ContentModelData metadata) => metadata != null && metadata.ContainsKey("ugcConfig")
             ? (ContentModelData) metadata["ugcConfig"]
