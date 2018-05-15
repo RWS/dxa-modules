9dcdf52c529a41e4c98d1098f5917de5b3b956e4
Paul Adams <majiccode@gmail.com>
2018-05-15 14:48:02 +0100

    TSI-3214 : Fix conditions format

--- webapp-net/TridionDocs/Providers/ConditionProvider.cs
+++ webapp-net/TridionDocs/Providers/ConditionProvider.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using Sdl.Web.Common;
 using Tridion.ContentDelivery.Meta;
 
@@ -13,21 +14,27 @@ namespace Sdl.Web.Modules.TridionDocs.Providers
         private static readonly string ConditionMetadata = "conditionmetadata.generated.value";
         private static readonly string ConditionValues = "values";
 
+        private class Condition
+        {
+            [JsonProperty("datatype")]
+            public string Datatype { get; set; }
+            [JsonProperty("range")]
+            public bool Range { get; set; }
+            [JsonProperty("values")]
+            public string[] Values { get; set; }
+        }
+
         public string GetConditions(int publicationId)
         {
             var conditionUsed = GetMetadata(publicationId, ConditionUsed);
             var conditionMetadata = GetMetadata(publicationId, ConditionMetadata);
-
-            JObject o1 = JObject.Parse(conditionUsed);
-            JObject o2 = JObject.Parse(conditionMetadata);
-
-            o1.Merge(o2, new JsonMergeSettings
+            Dictionary<string, string[]> d1 = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(conditionUsed);
+            Dictionary<string, Condition> d2 = JsonConvert.DeserializeObject<Dictionary<string, Condition>>(conditionMetadata);
+            foreach (var v in d1)
             {
-                // union array values together to avoid duplicates
-                MergeArrayHandling = MergeArrayHandling.Union
-            });
-
-            return o1.ToString();
+                d2[v.Key].Values = v.Value;
+            }
+            return JsonConvert.SerializeObject(d2);
         }
 
         private string GetMetadata(int publicationId, string metadataName)
