fff6067c28d8e23ed5d6971e9dff403572e7c275
Paul Adams <majiccode@gmail.com>
2018-05-18 12:26:37 +0100

    TSI-3218 : Tweak of search module result set

--- webapp-net/Search/Data/SearchResultSet.cs
+++ webapp-net/Search/Data/SearchResultSet.cs
@@ -16,35 +16,20 @@ namespace Sdl.Web.Modules.Search.Data
 
     public class SearchResultSetWrapped : IQueryResultData<SearchResultWrapped>
     {
-        private readonly SearchResultSet _searchResultSet;
         private readonly List<SearchResultWrapped> _results;
         public SearchResultSetWrapped(SearchResultSet searchResultSet)
         {
-            _searchResultSet = searchResultSet;
             _results = new List<SearchResultWrapped>();
-            foreach (var x in _searchResultSet.QueryResults)
+            foreach (var x in searchResultSet.QueryResults)
             {
                 _results.Add(new SearchResultWrapped(x));
             }
         }
 
-        public int Hits
-        {
-            get { return _searchResultSet.Hits; }
-            set { }
-        }
+        public int Hits { get; set; }
+        public int Count { get; set; }
+        public int StartIndex { get; set; }
 
-        public int Count
-        {
-            get { return _searchResultSet.Count; }
-            set { }
-        }
-
-        public int StartIndex
-        {
-            get { return _searchResultSet.StartIndex; }
-            set { }
-        }
         public IList<SearchResultWrapped> QueryResults
         {
             get { return _results; }
