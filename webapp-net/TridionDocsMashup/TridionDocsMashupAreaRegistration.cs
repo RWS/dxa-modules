92c7444adfdad83ea9951a3cd8d92a3bdfaf24d3
Ricardo Schroeder <rischroeder@sdl.com>
2018-06-19 12:44:37 +0200

    Renamed module ContentMashups to TridionDocsMashup.

---
+++ webapp-net/TridionDocsMashup/TridionDocsMashupAreaRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Sdl.Web.Modules.TridionDocsMashup.Models;
+using Sdl.Web.Mvc.Configuration;
+
+namespace Sdl.Web.Modules.TridionDocsMashup
+{
+    public class TridionDocsMashupAreaRegistration : BaseAreaRegistration
+    {
+        public override string AreaName => "TridionDocsMashup";
+
+        protected override void RegisterAllViewModels()
+        {
+            RegisterViewModel("DocsContent", typeof(DocsContent), "TridionDocsMashup");
+        }
+    }
+}
