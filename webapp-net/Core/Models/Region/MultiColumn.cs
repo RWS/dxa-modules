b309d58b1316e0ce5e38307b33fd2c666c72171e
Oleksii Shapovalov <oshapovalov@sdl.com>
2018-07-31 13:46:30 +0300

    CMF1-1333 Renamed entity name

--- webapp-net/Core/Models/Region/MultiColumn.cs
+++ webapp-net/Core/Models/Region/MultiColumn.cs
@@ -7,7 +7,7 @@ using Sdl.Web.Common.Models;
 
 namespace Sdl.Web.Modules.Core.Models
 {
-    [SemanticEntity(Vocab = SchemaOrgVocabulary, EntityName = "Multi-Column", Prefix = "s", Public = true)]
+    [SemanticEntity(Vocab = SchemaOrgVocabulary, EntityName = "[Multi-Column] Region", Prefix = "s", Public = true)]
     [Serializable]
     public class MultiColumn : RegionModel
     {
