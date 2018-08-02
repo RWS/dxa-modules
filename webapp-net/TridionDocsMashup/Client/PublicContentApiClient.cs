df29aeccad64341571b80a64c2630320f4ca7b92
Hamed Riahi <hriahi@sdl.com>
2018-08-02 11:34:17 +0200

    Try to convert the json to dd4t component model

--- webapp-net/TridionDocsMashup/Client/PublicContentApiClient.cs
+++ webapp-net/TridionDocsMashup/Client/PublicContentApiClient.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using DD4T.Serialization;
+using Newtonsoft.Json.Linq;
 using Sdl.Web.Common.Models;
 using Sdl.Web.Delivery.DiscoveryService;
 using Sdl.Web.GraphQLClient;
@@ -17,6 +18,7 @@ namespace Sdl.Web.Modules.TridionDocsMashup.Client
     public class PublicContentApiClient
     {
         private IPublicContentApi _publicContentApi;
+        private JSONSerializerService _dd4tSerializer = new JSONSerializerService();
 
         public PublicContentApiClient()
         {
@@ -24,7 +26,7 @@ namespace Sdl.Web.Modules.TridionDocsMashup.Client
             string graphQLEndpoint = "http://localhost:8081/udp/content";
 
             //todo : should be removed , as  PCA client will be provided and initialized by DXA , like SiteConfiguration.PublicContentApi;
-            IGraphQLClient graphQLClient = new GraphQLClient.GraphQLClient(graphQLEndpoint , new OAuth());
+            IGraphQLClient graphQLClient = new GraphQLClient.GraphQLClient(graphQLEndpoint, new OAuth());
 
             _publicContentApi = new PublicContentApi.PublicContentApi(graphQLClient);
         }
@@ -79,18 +81,18 @@ namespace Sdl.Web.Modules.TridionDocsMashup.Client
             return item;
         }
 
-        private List<DocsContent> ExtractDocsContent(ItemConnection itemConnection)
+        private List<DocsContent> ExtractDocsContent(ItemConnection item)
         {
-            if (itemConnection?.Edges == null)
+            if (item?.Edges == null)
             {
                 return null;
             }
 
             var docContents = new List<DocsContent>();
 
-            foreach (var item in itemConnection.Edges)
+            foreach (var edge in item.Edges)
             {
-                Page page = item.Node as Page;
+                Page page = edge.Node as Page;
 
                 if (page != null)
                 {
@@ -100,21 +102,12 @@ namespace Sdl.Web.Modules.TridionDocsMashup.Client
                     {
                         foreach (ComponentPresentation componentPresentation in page.ContainerItems)
                         {
-                            var component = componentPresentation?.RawContent?.Data["Component"] as JObject;
-                            if (component != null)
+                            string componentDD4TJson = (componentPresentation?.RawContent?.Data["Component"] as JObject)?.ToString();
+
+                            if (!string.IsNullOrEmpty(componentDD4TJson))
                             {
-                                var fields = component["Fields"] as JObject;
-                                if (fields != null)
-                                {
-                                    var sb = new StringBuilder();
-
-                                    foreach (var body in fields["topicBody"]["Values"])
-                                    {
-                                        sb.AppendLine(body.ToString());
-                                    }
-
-                                    docsContent.Content = sb.ToString();
-                                }
+                                DD4T.ContentModel.Component component = _dd4tSerializer.Deserialize<DD4T.ContentModel.Component>(componentDD4TJson);
+                                docsContent.Content = component?.Fields["topicBody"]?.Value;
                             }
                         }
                     }
