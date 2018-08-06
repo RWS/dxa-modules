68df9076144133575317e1e30f504a8dae5740ea
Igor Nikonov <inikonov@sdl.com>
2018-08-06 13:10:10 +0300

    TSI-3397 added needed dependency from smarttarget_core library

--- webapp-java/dxa-module-smarttarget/src/main/java/com/tridion/smarttarget/SmartTargetException.java
+++ webapp-java/dxa-module-smarttarget/src/main/java/com/tridion/smarttarget/SmartTargetException.java
@@ -1,5 +1,11 @@
 package com.tridion.smarttarget;
 
+/**
+ * This class is holder only. It's used instead of smartarget_core one
+ * in order to compile. It's deprecated because we have to remove it after
+ * moving to cil version 11.
+ */
+@Deprecated
 public class SmartTargetException extends Exception {
     public SmartTargetException() {
         super();
