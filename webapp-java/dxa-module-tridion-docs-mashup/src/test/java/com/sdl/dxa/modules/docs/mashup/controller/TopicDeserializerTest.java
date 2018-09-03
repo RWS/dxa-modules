51ea7b2cd6c2eb52fffc9f350fd47463dca3ce13
Ricardo Schroeder <rischroeder@sdl.com>
2018-09-03 12:40:52 +0200

    Included unit tests for link in topic deserializer

---
+++ webapp-java/dxa-module-tridion-docs-mashup/src/test/java/com/sdl/dxa/modules/docs/mashup/controller/TopicDeserializerTest.java
@@ -0,0 +1,47 @@
+package com.sdl.dxa.modules.docs.mashup.controller;
+
+import com.sdl.dxa.modules.docs.mashup.client.TopicDeserializer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.xml.bind.ValidationException;
+
+/**
+ * Test for TopicDeserializer class.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TopicDeserializerTest {
+    @Spy
+    TopicDeserializer topicDeserializer;
+    private ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void getLinkTests() {
+        // should be empty
+        Assert.assertEquals("", topicDeserializer.getLink(""));
+
+        // should be null
+        Assert.assertNull(topicDeserializer.getLink(null));
+
+        // should have the / appended at the beginning
+        String link = "link_without_slash_at_beginning";
+        Assert.assertEquals("/" + link, topicDeserializer.getLink(link));
+
+        // should not do anything to the link, just return it
+        link = "/link_with_slash_at_beginning";
+        Assert.assertEquals(link, topicDeserializer.getLink(link));
+
+        // should not do anything to the link, just return it
+        link = "http://www.url.com/link_with_slash_at_beginning";
+        Assert.assertEquals(link, topicDeserializer.getLink(link));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getLinkThrownWhenInvalidLink() {
+        Assert.assertNull(topicDeserializer.getLink("invalid_link_()*&@%#!?"));
+    }
+}
