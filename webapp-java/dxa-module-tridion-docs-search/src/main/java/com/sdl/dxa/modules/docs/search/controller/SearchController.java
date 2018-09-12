28ab92ff94b564a1314753716ba2f935bc91ba99
mborysenko <mborysenko@sdl.com>
2018-09-12 14:21:25 +0300

    TSI-3428 > removed redundant import

--- webapp-java/dxa-module-tridion-docs-search/src/main/java/com/sdl/dxa/modules/docs/search/controller/SearchController.java
+++ webapp-java/dxa-module-tridion-docs-search/src/main/java/com/sdl/dxa/modules/docs/search/controller/SearchController.java
@@ -7,7 +7,6 @@ import com.sdl.dxa.modules.docs.search.model.SearchResultSet;
 import com.sdl.dxa.modules.docs.search.service.SearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
