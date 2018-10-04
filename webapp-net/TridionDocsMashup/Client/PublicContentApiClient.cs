a6874ef41e46785b4363ff49b7dcbd6bd34102ec
Hamed Riahi <hriahi@sdl.com>
2018-10-04 16:13:08 +0200

    Remove url extra slashes (/)

--- webapp-net/TridionDocsMashup/Client/PublicContentApiClient.cs
+++ webapp-net/TridionDocsMashup/Client/PublicContentApiClient.cs
@@ -277,7 +277,7 @@ namespace Sdl.Web.Modules.TridionDocsMashup.Client
 
                     if (Uri.TryCreate(prefixForTopicsUrl, UriKind.RelativeOrAbsolute, out prefixUri))
                     {
-                        if (Uri.TryCreate(prefixUri.ToString() + "/" + url, UriKind.Absolute, out uri))
+                        if (Uri.TryCreate(prefixUri.ToString().TrimEnd('/') + url, UriKind.Absolute, out uri))
                         {
                             url = uri.ToString();
                         }
