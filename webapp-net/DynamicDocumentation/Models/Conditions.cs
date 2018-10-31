3d8ac3ddf95e6fcd9165847ae0dac44412fc9752
Paul Adams <majiccode@gmail.com>
2018-10-31 15:04:58 +0000

    TSI-3689 : Tweak to conditions

---
+++ webapp-net/DynamicDocumentation/Models/Conditions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sdl.Web.Common.Utils;
+
+namespace Sdl.Web.Modules.DynamicDocumentation.Models
+{
+    public class Conditions
+    {
+        public string PublicationId { get; set; }
+        public Dictionary<string, object> UserConditions { get; set; }
+        public override int GetHashCode()
+        {
+            int hash = (PublicationId ?? string.Empty).GetHashCode();
+            if (UserConditions != null)
+            {
+                hash = UserConditions.Aggregate(hash, (current, x) => Hash.CombineHashCodes(current, x.Key.GetHashCode(), x.Value.GetHashCode()));
+            }
+            return hash;
+        }
+    }
+}
