1b4bc70d4e89e4ab015acd2fd4782e840f3ce1a8
Paul Adams <majiccode@gmail.com>
2018-11-07 17:13:38 +0000

    TSI-3689 : fix

--- webapp-net/Ugc/Controllers/UgcApiController.cs
+++ webapp-net/Ugc/Controllers/UgcApiController.cs
@@ -73,7 +73,7 @@ namespace Sdl.Web.Modules.Ugc.Controllers
                     userId = "Anonymous";
                 }
 
-                Comment result = ugc.PostComment(posted.PublicationId.Value,
+                Comment result = ugc.PostComment(posted.PublicationId,
                     posted.PageId.Value,
                     userId,
                     posted.Email,
