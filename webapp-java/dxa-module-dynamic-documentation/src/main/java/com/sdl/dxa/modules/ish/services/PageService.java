be416eaea56a04d20d5d25a462cee1524749dc82
Maksym Borysenko <mborysenko@sdl.com>
2019-08-26 15:22:22 +0200

    CRQ-16059 > Made PageService interface use only interfaces instead of implementations

--- webapp-java/dxa-module-dynamic-documentation/src/main/java/com/sdl/dxa/modules/ish/services/PageService.java
+++ webapp-java/dxa-module-dynamic-documentation/src/main/java/com/sdl/dxa/modules/ish/services/PageService.java
@@ -2,10 +2,10 @@ package com.sdl.dxa.modules.ish.services;
 
 import com.sdl.webapp.common.api.content.ContentProviderException;
 import com.sdl.webapp.common.api.localization.Localization;
-import com.sdl.webapp.common.api.model.PageModel;
+import com.sdl.webapp.common.api.model.ViewModel;
 import org.springframework.cache.annotation.Cacheable;
 
 public interface PageService {
     @Cacheable("ish")
-    PageModel getPage(int pageId, Localization localization) throws ContentProviderException;
+    ViewModel getPage(int pageId, Localization localization) throws ContentProviderException;
 }
