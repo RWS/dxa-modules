29825bfaad4dfad4729cf39aadb5a18f19d16c1f
Igor Nikonov <inikonov@sdl.com>
2019-10-25 16:07:24 +0300

    CRQ-16672 added possibility to search for both UDP 11 and 11.1 version: UDP 11.1 has changed content.japanese => content.cjk for CJK languages

--- webapp-java/dxa-module-tridion-docs-search/src/test/java/com/sdl/dxa/modules/docs/search/service/SearcherConfigurerTest.java
+++ webapp-java/dxa-module-tridion-docs-search/src/test/java/com/sdl/dxa/modules/docs/search/service/SearcherConfigurerTest.java
@@ -60,6 +60,51 @@ public class SearcherConfigurerTest {
         assertEquals(expected, criteria.getRawQuery());
     }
 
+    @Test
+    public void testBuildCriteriaForJapanese() throws SearchException {
+        final String expected = "{\"type\":\"query\",\"op\":\"AND\",\"nodes\":[{\"type\":\"query\",\"op\":\"AND\"," +
+                "\"nodes\":[{\"type\":\"field\",\"key\":\"dynamic.FISHDITADLVRREMOTESTATUS.lng.element\"," +
+                "\"value\":\"VDITADLVRREMOTESTATUSONLINE\",\"fieldType\":\"STRING\"},{\"type\":\"field\"," +
+                "\"key\":\"publicationId\",\"value\":\"123123\",\"termType\":\"EXACT\",\"fieldType\":" +
+                "\"INTEGER\"}]},{\"type\":\"query\",\"op\":\"OR\",\"nodes\":[{\"type\":\"field\",\"key\":" +
+                "\"content.cjk\",\"value\":\"some query\",\"fieldType\":\"STRING\"},{\"type\":\"field\",\"key\":" +
+                "\"content.japanese\",\"value\":\"some query\",\"fieldType\":\"STRING\"}]}]}";
+        SearchParameters parameters = new SearchParameters();
+        parameters.setPublicationId(PUBLICATION_ID);
+        parameters.setSearchQuery("\"some query\"");
+        parameters.setLanguage("ja-jp");
+        Criteria criteria = configurer.buildCriteria(parameters);
+        assertEquals(expected, criteria.getRawQuery());
+    }
+
+    @Test
+    public void testBuildCriteriaForChinese() throws SearchException {
+        final String expected = "{\"type\":\"query\",\"op\":\"AND\",\"nodes\":[{\"type\":\"field\",\"key\":" +
+                "\"dynamic.FISHDITADLVRREMOTESTATUS.lng.element\",\"value\":\"VDITADLVRREMOTESTATUSONLINE\"," +
+                "\"fieldType\":\"STRING\"},{\"type\":\"query\",\"op\":\"OR\",\"nodes\":[{\"type\":\"field\"," +
+                "\"key\":\"content.cjk\",\"value\":\"some query\",\"fieldType\":\"STRING\"},{\"type\":\"field\"," +
+                "\"key\":\"content.chinese\",\"value\":\"some query\",\"fieldType\":\"STRING\"}]}]}";
+        SearchParameters parameters = new SearchParameters();
+        parameters.setSearchQuery("\"some query\"");
+        parameters.setLanguage("zh-tw");
+        Criteria criteria = configurer.buildCriteria(parameters);
+        assertEquals(expected, criteria.getRawQuery());
+    }
+
+    @Test
+    public void testBuildCriteriaForKorean() throws SearchException {
+        final String expected = "{\"type\":\"query\",\"op\":\"AND\",\"nodes\":[{\"type\":\"field\",\"key\":" +
+                "\"dynamic.FISHDITADLVRREMOTESTATUS.lng.element\",\"value\":\"VDITADLVRREMOTESTATUSONLINE\"," +
+                "\"fieldType\":\"STRING\"},{\"type\":\"query\",\"op\":\"OR\",\"nodes\":[{\"type\":\"field\"," +
+                "\"key\":\"content.cjk\",\"value\":\"some query\",\"fieldType\":\"STRING\"},{\"type\":\"field\"," +
+                "\"key\":\"content.korean\",\"value\":\"some query\",\"fieldType\":\"STRING\"}]}]}";
+        SearchParameters parameters = new SearchParameters();
+        parameters.setSearchQuery("\"some query\"");
+        parameters.setLanguage("ko-KO");
+        Criteria criteria = configurer.buildCriteria(parameters);
+        assertEquals(expected, criteria.getRawQuery());
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testBuildCriteriaIncorrectParameters() throws SearchException {
         SearchParameters parameters = new SearchParameters();
